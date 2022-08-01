e70cd18d0910df7c6325ce297b2b2302a24f8d1a
Yongfeng LI <wliyongfeng@gmail.com>
2022-08-01 16:05:44 +0800

    Create test case for extractCalls

--- backend/packages/block-scan/src/scan/call/exempt.js
+++ backend/packages/block-scan/src/scan/call/exempt.js
@@ -1,7 +1,31 @@
-const { consts: { Modules } } = require("@osn/scan-common");
+const {
+  consts: {
+    Modules,
+    ProxyMethods,
+    MultisigMethods,
+    UtilityMethods,
+    SudoMethods,
+  }
+} = require("@osn/scan-common");
 
 function isExemptedCall(section, method) {
   if ([Modules.Timestamp, Modules.ParachainSystem, Modules.ParaInherent].includes(section)) {
+    return true;
+  }
+
+  if (
+    (Modules.Proxy === section && ProxyMethods.proxy === method) ||
+    (Modules.Multisig === section && MultisigMethods.asMulti) ||
+    (Modules.Utility === section &&
+      [
+        UtilityMethods.batch,
+        UtilityMethods.batchAll,
+      ].includes(method)) ||
+    (Modules.Sudo === section && [
+      SudoMethods.sudo,
+      SudoMethods.sudoAs,
+    ].includes(method))
+  ) {
     return true;
   }
 
