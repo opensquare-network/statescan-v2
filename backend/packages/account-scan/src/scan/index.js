d8a6b5c5cb3fc9faa9e9b44c955e70c452104b24
Yongfeng LI <wliyongfeng@gmail.com>
2022-09-07 23:42:56 +0800

    Update block accounts

--- backend/packages/account-scan/src/scan/index.js
+++ backend/packages/account-scan/src/scan/index.js
@@ -1,3 +1,6 @@
+const { bulkUpdateAccounts } = require("../mongo/services/bulkUpdate");
+const { getOnChainAccounts } = require("../common/getOnChainAccounts");
+const { getAddresses } = require("./store/address");
 const {
   chain: {
     getBlockIndexer,
@@ -15,12 +18,23 @@ const {
   }
 } = require("@osn/scan-common");
 
+async function updateBlockAccounts(height) {
+  const addrs = getAddresses(height)
+  const onChainDataArr = await getOnChainAccounts(addrs);
+  if (onChainDataArr.length <= 0) {
+    return
+  }
+
+  await bulkUpdateAccounts(onChainDataArr);
+}
+
 async function handleBlock({ block, author, events, height }) {
   const blockIndexer = getBlockIndexer(block);
   // todo: handle account related business in block
 
   console.log('blockIndexer', blockIndexer);
 
+  await updateBlockAccounts(height);
   await updateScanHeight(height);
 }
 
