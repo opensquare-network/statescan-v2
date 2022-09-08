3457aa5d6df361110e66e0e9d443b6ab9b762bfb
yoshiyuki <brother-zhou@qq.com>
2022-09-08 09:52:17 +0800

    feat: init site (#3)
    * feat: init site
    
    * doc: site readme

---
+++ site/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Statescan-v2</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
