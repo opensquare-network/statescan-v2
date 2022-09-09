31ca1580e97f07b3d77f05ac8325ad533c85520f
yoshiyuki <brother-zhou@qq.com>
2022-09-09 17:58:37 +0800

    init: router

---
+++ site/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+
+      <div className="App">
+        <header className="App-header">
+          <h1>Statescan-v2</h1>
+        </header>
+      </div>
+  );
+}
+
+export default Home;
