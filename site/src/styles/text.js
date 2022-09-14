64fbd7c31f6ccc902484eae3a154f21896e88fa0
yoshiyuki <brother-zhou@qq.com>
2022-09-14 14:52:37 +0800

    style: add fonts & install styled-components

---
+++ site/src/styles/text.js
@@ -0,0 +1,92 @@
+import { css } from "styled-components";
+
+const Inter = css`
+  font-family: Inter, sans-serif;
+  font-style: normal;
+`;
+
+export const Inter_40_700 = css`
+  ${Inter};
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 40px;
+`;
+
+export const Inter_24_700 = css`
+  ${Inter};
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 24px;
+`;
+
+export const Inter_20_700 = css`
+  ${Inter};
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 20px;
+`;
+
+export const Inter_14_500 = css`
+  ${Inter};
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+export const Inter_14_400 = css`
+  ${Inter};
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+export const Inter_12_500 = css`
+  ${Inter};
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+`;
+
+export const Inter_12_400 = css`
+  ${Inter};
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+`;
+
+const SF_Mono = css`
+  font-family: SF Mono, sans-serif;
+  font-style: normal;
+`;
+
+export const SF_Mono_14_500 = css`
+  ${SF_Mono};
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: -0.2px;
+`;
+
+export const SF_Mono_14_400 = css`
+  ${SF_Mono};
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: -0.2px;
+`;
+
+export const SF_Mono_12_500 = css`
+  ${SF_Mono};
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: -0.2px;
+`;
+
+export const SF_Mono_12_400 = css`
+  ${SF_Mono};
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: -0.2px;
+`;
