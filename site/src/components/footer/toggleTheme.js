f3abcb360e292ac584837e337c2f916e7ddaaede
yoshiyuki <brother-zhou@qq.com>
2022-09-19 17:33:14 +0800

    style: darkmode

--- site/src/components/footer/toggleTheme.js
+++ site/src/components/footer/toggleTheme.js
@@ -1,23 +1,35 @@
-import Switch from "../styled/switch";
 import { ReactComponent as SunIcon } from "./sun.svg";
 import { ReactComponent as MoonIcon } from "./moon.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { modeSelector, setMode } from "../../store/reducers/modeSlice";
+import styled from "styled-components";
+import { Flex } from "../styled/flex";
+
+const Wrapper = styled(Flex)`
+  gap: 24px;
+
+  svg {
+    cursor: pointer;
+  }
+`;
+const Divider = styled.div`
+  width: 1px;
+  height: 12px;
+  background: ${(props) => props.theme.strokeBox};
+`;
 
 export default function ToggleTheme() {
   const dispatch = useDispatch();
   const mode = useSelector(modeSelector);
-  const Icon = mode === "light" ? SunIcon : MoonIcon;
 
   return (
-    <Switch
-      on={mode === "dark"}
-      onToggle={(on) => {
-        dispatch(setMode(on ? "dark" : "light"));
+    <Wrapper
+      onClick={() => {
+        dispatch(setMode(mode === "light" ? "dark" : "light"));
       }}
     >
-      {" "}
-      <Icon />
-    </Switch>
+      <Divider />
+      {mode === "light" ? <MoonIcon /> : <SunIcon />}
+    </Wrapper>
   );
 }
