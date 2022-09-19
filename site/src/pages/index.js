0833db9302d2a28996300ab90cc5c7c44901e824
yoshiyuki <brother-zhou@qq.com>
2022-09-19 08:48:53 +0800

    stash: home blocks

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../components/layout";
 import Explore from "../components/home/explore";
+import Sections from "../components/home/sections";
 
 function Blocks() {
   return (
     <Layout>
       <Explore />
+      <Sections />
     </Layout>
   );
 }
--- site/src/styles/text.js
+++ site/src/styles/text.js
@@ -19,7 +19,7 @@ export const Inter_24_700 = css`
   line-height: 24px;
 `;
 
-export const Inter_20_700 = css`
+export const Inter_18_700 = css`
   ${Inter};
   font-weight: 700;
   font-size: 20px;
