1086143ee78c099f8c63ad0408836d5f33c6e5fe
yoshiyuki <brother-zhou@qq.com>
2022-09-26 17:43:21 +0800

    fix: backend page count from zero

--- site/src/utils/viewFuncs/index.js
+++ site/src/utils/viewFuncs/index.js
@@ -1,3 +1,5 @@
+import * as queryString from "query-string";
+
 export function addressEllipsis(address, start = 4, end = 4) {
   if (!address) return;
   if (address.length <= start + end) return address;
@@ -9,4 +11,8 @@ export default function encodeUriQuery(q) {
   return Object.keys(q)
     .map((k) => `${k}=${encodeURIComponent(q[k])}`)
     .join("&");
+}
+
+export function getPageFromQuery(location) {
+  return parseInt(queryString.parse(location.search)?.page ?? 1);
 }
