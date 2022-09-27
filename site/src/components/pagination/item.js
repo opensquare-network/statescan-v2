b671b96f449490a893ad81c5296b1bc479ef6426
yoshiyuki <brother-zhou@qq.com>
2022-09-27 13:38:28 +0800

    refactor: pagination new design

--- site/src/components/pagination/item.js
+++ site/src/components/pagination/item.js
@@ -1,52 +1,19 @@
-import React from "react";
-import Link from "../styled/link";
-import styled, { css } from "styled-components";
-import encodeUriQuery from "../../utils/viewFuncs";
-import { Flex } from "../styled/flex";
 import { Inter_14_500 } from "../../styles/text";
+import styled from "styled-components";
+import { Flex } from "../styled/flex";
+import React from "react";
 
 const Item = styled(Flex)`
   box-sizing: border-box;
   padding: 0 8px;
-  cursor: pointer;
   min-width: 28px;
   height: 28px;
   border-radius: 4px;
   justify-content: center;
   ${Inter_14_500};
   color: ${(props) => props.theme.fontSecondary};
-
-  :hover {
-    color: ${(props) => props.theme.theme500};
-  }
-
-  ${(p) =>
-    p.active &&
-    css`
-      background: ${(props) => props.theme.theme100};
-      color: ${(props) => props.theme.theme500};
-      cursor: auto;
-      pointer-events: none;
-    `}
 `;
 
-export default function PageItem({ page, now, onPageChange = null }) {
-  return (
-    <Link
-      key={page}
-      to={`?${encodeUriQuery({
-        ...{},
-        page: page + 1,
-      })}`}
-    >
-      <Item
-        active={parseInt(now) === parseInt(page + 1)}
-        onClick={(e) => {
-          onPageChange && onPageChange(e, page + 1);
-        }}
-      >
-        {page + 1}
-      </Item>
-    </Link>
-  );
+export default function PageItem({ page, now }) {
+  return <Item>{page + 1}</Item>;
 }
