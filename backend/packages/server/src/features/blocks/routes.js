a2245dfc26ba16ae0ecb2e0403525eb9a821273a
Yongfeng LI <wliyongfeng@gmail.com>
2022-09-27 17:23:04 +0800

    Add block events api

--- backend/packages/server/src/features/blocks/routes.js
+++ backend/packages/server/src/features/blocks/routes.js
@@ -1,4 +1,5 @@
 const Router = require("koa-router");
+const { getBlockEvents } = require("./controllers/blockEvents");
 const { getBlockExtrinsics } = require("./controllers/blockExtrinsics");
 const { getBlock } = require("./controllers/block");
 const { getBlocks } = require("./controllers/blocks");
@@ -7,5 +8,6 @@ const router = new Router();
 router.get("/blocks", getBlocks);
 router.get("/blocks/:heightOrHash", getBlock);
 router.get("/blocks/:heightOrHash/extrinsics", getBlockExtrinsics);
+router.get("/blocks/:heightOrHash/events", getBlockEvents);
 
 module.exports = router;
