707067a4667e64a33225da26667b2cf0e53baaaf
Yongfeng LI <wliyongfeng@gmail.com>
2022-09-29 14:48:35 +0800

    Add api to get extrinsic events

--- backend/packages/server/src/features/extrinsics/routes.js
+++ backend/packages/server/src/features/extrinsics/routes.js
@@ -1,4 +1,5 @@
 const Router = require("koa-router");
+const { getExtrinsicEvents } = require("./controllers/extrinsicEvents");
 const { getExtrinsicCalls } = require("./controllers/calls");
 const { getExtrinsic } = require("./controllers/extrinsic");
 const { getExtrinsics } = require("./controllers/extrinsics");
@@ -7,5 +8,9 @@ const router = new Router();
 router.get("/extrinsics", getExtrinsics);
 router.get("/extrinsics/:indexOrHash", getExtrinsic);
 router.get("/extrinsics/:blockHeight-:extrinsicIndex/calls", getExtrinsicCalls);
+router.get(
+  "/extrinsics/:blockHeight-:extrinsicIndex/events",
+  getExtrinsicEvents,
+);
 
 module.exports = router;
