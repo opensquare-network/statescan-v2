cc707df70f5d84aedb352573e77bf3306eb9bc89
2nthony <ant.hu@outlook.com>
2022-10-11 15:37:34 +0800

    refactor: hooks

--- site/src/utils/hooks/index.js
+++ site/src/utils/hooks/index.js
@@ -1,54 +1,12 @@
-import { useEffect, useRef, useState, useCallback, useMemo } from "react";
+import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { modeSelector } from "../../store/reducers/settingSlice";
 
-export function useOnClickOutside(ref, handler) {
-  useEffect(() => {
-    const listener = (event) => {
-      if (!ref.current || ref.current.contains(event.target)) {
-        return;
-      }
-      handler(event);
-    };
-    document.addEventListener("mousedown", listener);
-    document.addEventListener("touchstart", listener);
-    return () => {
-      document.removeEventListener("mousedown", listener);
-      document.removeEventListener("touchstart", listener);
-    };
-  });
-}
-
-export function useIsMounted() {
-  const isMountedRef = useRef(true);
-  const isMounted = useCallback(() => isMountedRef.current, []);
-  useEffect(() => {
-    return () => void (isMountedRef.current = false);
-  }, []);
-  return isMounted;
-}
-
-export function useWindowSize() {
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
-  });
-  useEffect(() => {
-    function handleResize() {
-      setWindowSize({
-        // width: window.screen.width,
-        // height: window.screen.height,
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    }
-
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-  return windowSize;
-}
+export {
+  useOnClickOutside,
+  useIsMounted,
+  useWindowSize,
+} from "@osn/common/hooks";
 
 export function useIsDark() {
   const mode = useSelector(modeSelector);
