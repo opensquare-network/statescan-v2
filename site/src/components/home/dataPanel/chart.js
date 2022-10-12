b7aeb54e56a26a73a0409108afc77236a8c8a544
2nthony <ant.hu@outlook.com>
2022-10-12 17:27:41 +0800

    feat: data panel chart init

--- site/src/components/home/dataPanel/chart.js
+++ site/src/components/home/dataPanel/chart.js
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+import { chainSettingSelector } from "../../../store/reducers/settingSlice";
+import LineChart from "../../charts/lineChart";
+
 function DataPanelChart() {
-  return <div>data panel chart</div>;
+  const chainSetting = useSelector(chainSettingSelector);
+
+  return <LineChart token={chainSetting.symbol} />;
 }
 
 export default DataPanelChart;
