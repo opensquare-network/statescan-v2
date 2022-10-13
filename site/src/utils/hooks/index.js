c2ca16bd09041ba5e2b446c32f547454c87f7148
2nthony <ant.hu@outlook.com>
2022-10-13 18:05:31 +0800

    use is screen hook

--- site/src/utils/hooks/index.js
+++ site/src/utils/hooks/index.js
@@ -1,16 +1,31 @@
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { modeSelector } from "../../store/reducers/settingSlice";
-
-export {
+import {
   useOnClickOutside,
   useIsMounted,
   useWindowSize,
 } from "@osn/common/hooks";
+import { MOBILE_SIZE } from "../constants";
+
+export { useOnClickOutside, useIsMounted, useWindowSize };
 
 export function useIsDark() {
   const mode = useSelector(modeSelector);
   const isDark = useMemo(() => mode === "dark", [mode]);
 
   return isDark;
+}
+
+/**
+ * @description `duplicated` osn/common but with different breakpoint
+ * @breakpoint 600px
+ */
+export function useIsScreen() {
+  const { width } = useWindowSize();
+
+  return {
+    isDesktop: width > MOBILE_SIZE,
+    isMobile: width <= MOBILE_SIZE,
+  };
 }
