3d64178a9bfd7cff269759043c2dd88330ab848e
2nthony <ant.hu@outlook.com>
2022-10-14 14:52:51 +0800

    refactor: themed chain icons (#163)
    * refactor: themed chain icons
    
    * refactor: use themed chain icons
    
    * refactor: remove fragment
    
    * refactor: no chain icons hook needed

---
+++ site/src/components/icons/westmintIcon.js
@@ -0,0 +1,10 @@
+import { ReactComponent as Westmint } from "./westmint.svg";
+import { ReactComponent as WestmintDark } from "./westmint-dark.svg";
+import { useIsDark } from "../../utils/hooks";
+import { useMemo } from "react";
+
+export default function WestmintIcon(props) {
+  const isDark = useIsDark();
+  const Icon = useMemo(() => (isDark ? WestmintDark : Westmint), [isDark]);
+  return <Icon {...props} />;
+}
