fceafd9f3e3910b1779689a9f791e4a1483715e6
2nthony <ant.hu@outlook.com>
2022-10-14 15:52:51 +0800

    refactor: move mobile menu to header itself

--- site/src/components/layout/index.js
+++ site/src/components/layout/index.js
@@ -7,12 +7,7 @@ import light from "../../styles/theme/light";
 import Background from "../dotBackground";
 import Footer from "../footer";
 import dark from "../../styles/theme/dark";
-import ChainSwitch from "../header/chainSwitch";
-import {
-  closeMobileMenu,
-  mobileMenuFoldedSelector,
-} from "../../store/reducers/mobileMenuSlice";
-import Navi from "../header/navi";
+import { closeMobileMenu } from "../../store/reducers/mobileMenuSlice";
 import { useWindowSize } from "../../utils/hooks";
 import { useEffect } from "react";
 import Tip from "../tooltip/tip";
@@ -31,25 +26,9 @@ const Main = styled.main`
   z-index: 1;
 `;
 
-const MobileMenuWrapper = styled.div`
-  padding: 0 24px;
-  @media screen and (max-width: 600px) {
-    padding: 0 16px;
-  }
-  box-sizing: border-box;
-  position: fixed !important;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  overflow: scroll;
-  z-index: 2;
-`;
-
 export default function Layout({ children }) {
   const mode = useSelector(modeSelector);
   const theme = mode === "light" ? light : dark;
-  const showMobileMenu = !useSelector(mobileMenuFoldedSelector);
   const tooltipContent = useSelector(tooltipContentSelector);
   const dispatch = useDispatch();
 
@@ -71,13 +50,6 @@ export default function Layout({ children }) {
         <Main>
           <Container>{children}</Container>
         </Main>
-        {showMobileMenu && (
-          <MobileMenuWrapper>
-            <Header />
-            <ChainSwitch />
-            <Navi />
-          </MobileMenuWrapper>
-        )}
         <Footer />
         <Tip>{tooltipContent}</Tip>
       </Wrapper>
