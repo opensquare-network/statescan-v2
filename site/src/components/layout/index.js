53a2c8b08c35eb01a8e88fa0f4f213739f46077f
2nthony <ant.hu@outlook.com>
2022-10-14 15:56:17 +0800

    chore: clean mobile menu effect

--- site/src/components/layout/index.js
+++ site/src/components/layout/index.js
@@ -1,15 +1,12 @@
 import styled, { ThemeProvider } from "styled-components";
 import Container from "./container";
 import Header from "../header";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { modeSelector } from "../../store/reducers/settingSlice";
 import light from "../../styles/theme/light";
 import Background from "../dotBackground";
 import Footer from "../footer";
 import dark from "../../styles/theme/dark";
-import { closeMobileMenu } from "../../store/reducers/mobileMenuSlice";
-import { useWindowSize } from "../../utils/hooks";
-import { useEffect } from "react";
 import Tip from "../tooltip/tip";
 import { tooltipContentSelector } from "../../store/reducers/tooltipSlice";
 
@@ -30,15 +27,6 @@ export default function Layout({ children }) {
   const mode = useSelector(modeSelector);
   const theme = mode === "light" ? light : dark;
   const tooltipContent = useSelector(tooltipContentSelector);
-  const dispatch = useDispatch();
-
-  const { width } = useWindowSize();
-
-  useEffect(() => {
-    if (width > 600) {
-      dispatch(closeMobileMenu());
-    }
-  }, [dispatch, width]);
 
   return (
     <ThemeProvider theme={theme}>
