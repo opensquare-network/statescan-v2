07bd59dc02dc2bc3c5bf1b1a1220ae136f0024e2
2nthony <ant.hu@outlook.com>
2022-10-17 20:28:59 +0800

    feat: create detail page table

---
+++ site/src/components/detail/createDetailTable.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import api from "../../services/api";
+import { LIST_DEFAULT_PAGE_SIZE } from "../../utils/constants";
+import { getPageFromQuery } from "../../utils/viewFuncs";
+import Pagination from "../pagination";
+import { StyledPanelTableWrapper } from "../styled/panel";
+import Table from "../table";
+import { noop } from "lodash";
+
+export function createDetailTable({
+  url = "",
+  heads = [],
+  transformData = noop,
+  TableComponent,
+}) {
+  return (
+    <DetailTable
+      url={url}
+      heads={heads}
+      transformData={transformData}
+      TableComponent={TableComponent}
+    />
+  );
+}
+
+function DetailTable({ url, heads, transformData = noop, TableComponent }) {
+  const location = useLocation();
+  const [data, setData] = useState(null);
+  const [total, setTotal] = useState(0);
+  const page = getPageFromQuery(location);
+  const pageSize = LIST_DEFAULT_PAGE_SIZE;
+
+  useEffect(() => {
+    if (!url) {
+      return;
+    }
+
+    setData(null);
+    api
+      .fetch(url, {
+        page: page - 1,
+        pageSize,
+      })
+      .then(({ result }) => {
+        setData(result?.items ?? []);
+        setTotal(result?.total ?? 0);
+      });
+  }, [url, page, pageSize]);
+
+  return (
+    <StyledPanelTableWrapper>
+      {TableComponent ? (
+        <TableComponent data={data} />
+      ) : (
+        <Table heads={heads} data={transformData(data)} />
+      )}
+      <Pagination page={page} pageSize={pageSize} total={total} />
+    </StyledPanelTableWrapper>
+  );
+}
