a33cea314055f411250a5e42e66a78ff6158500f
Yongfeng LI <wliyongfeng@gmail.com>
2022-10-17 22:21:43 +0800

    Save callsCount to extrinsic detail, #179

--- backend/packages/block-scan/src/scan/extrinsic/call/index.js
+++ backend/packages/block-scan/src/scan/extrinsic/call/index.js
@@ -1,7 +1,6 @@
 const { isExemptedCall } = require("./exempt");
 const {
   handleCallsInExtrinsic,
-  utils: { extractExtrinsicEvents, isExtrinsicSuccess },
   call: { normalizeCall },
 } = require("@osn/scan-common");
 
@@ -11,14 +10,14 @@ let extrinsicCalls = [];
 async function handleCall(call, author, extrinsicIndexer, wrappedEvents) {
   const { section, method } = call;
   if (isExemptedCall(section, method)) {
-    return
+    return;
   }
 
   const normalizedCall = normalizeCall(call);
   const indexer = {
     ...extrinsicIndexer,
     callIndex: extrinsicCallIndex,
-  }
+  };
 
   let eventAttributes = {
     isCall: false,
@@ -26,7 +25,7 @@ async function handleCall(call, author, extrinsicIndexer, wrappedEvents) {
   if (wrappedEvents.wrapped) {
     eventAttributes.isCall = true;
     eventAttributes.offset = wrappedEvents.offset;
-    eventAttributes.offset = wrappedEvents.events.length;
+    eventAttributes.length = wrappedEvents.events.length;
   }
 
   extrinsicCalls.push({
@@ -38,26 +37,13 @@ async function handleCall(call, author, extrinsicIndexer, wrappedEvents) {
   extrinsicCallIndex++;
 }
 
-async function extractCalls(extrinsics, allEvents = [], blockIndexer) {
+async function extractCallsFromExtrinsic(extrinsic, events, extrinsicIndexer) {
+  extrinsicCallIndex = 0;
   extrinsicCalls = [];
-  let index = 0;
-  for (const extrinsic of extrinsics) {
-    extrinsicCallIndex = 0;
-    const events = extractExtrinsicEvents(allEvents, index);
-    if (!isExtrinsicSuccess(events)) {
-      continue;
-    }
-
-    // todo: filter out extrinsics which don't contain business calls
-    //  sections include: timestamp, parachainSystem, parainherent, parainclusion, parascheduler,
-
-    const extrinsicIndexer = { ...blockIndexer, extrinsicIndex: index++ };
-    await handleCallsInExtrinsic(extrinsic, events, extrinsicIndexer, handleCall)
-  }
-
-  return [...extrinsicCalls];
+  await handleCallsInExtrinsic(extrinsic, events, extrinsicIndexer, handleCall);
+  return extrinsicCalls;
 }
 
 module.exports = {
-  extractCalls,
-}
+  extractCallsFromExtrinsic,
+};
