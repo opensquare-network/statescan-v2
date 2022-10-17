d4025d24303d4c93d2e92f2033aa24e18986fdec
2nthony <ant.hu@outlook.com>
2022-10-17 20:26:45 +0800

    fix: remove strict mode

--- site/src/index.js
+++ site/src/index.js
@@ -8,11 +8,9 @@ import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
