47d560fc392e929c094fc9780bb816a7f5f6beee
yoshiyuki <brother-zhou@qq.com>
2022-10-18 17:21:00 +0800

    feat: apply 404 feat to more pages

--- site/src/utils/viewFuncs/toDetailItem.js
+++ site/src/utils/viewFuncs/toDetailItem.js
@@ -5,9 +5,28 @@ import React from "react";
 import { withCopy } from "../../HOC/withCopy";
 import { TextSecondary } from "../../components/styled/text";
 import { ColoredMonoLink } from "../../components/styled/link";
+import Tooltip from "../../components/tooltip";
+import { toPrecision } from "./index";
+import ValueDisplay from "../../components/displayValue";
+import DetailedCallId from "../../components/detail/callId";
+import DetailedExtrinsicId from "../../components/detail/extrinsicId";
+import DetailedBlock from "../../components/detail/block";
+import styled from "styled-components";
+import { Tag as TagOrigin } from "../../components/tag";
+import { Inter_14_500 } from "../../styles/text";
 
 const TextSecondaryWithCopy = withCopy(TextSecondary);
 const ColoredMonoLinkWithCopy = withCopy(ColoredMonoLink);
+
+const Tag = styled(TagOrigin)`
+  color: ${(p) => p.theme.fontSecondary};
+  background-color: ${(p) => p.theme.fillBase};
+`;
+
+const CallText = styled.span`
+  color: ${(p) => p.theme.fontSecondary};
+  ${Inter_14_500};
+`;
 
 export const toBlockDetailItem = (block) => {
   return {
@@ -28,5 +47,82 @@ export const toBlockDetailItem = (block) => {
       <TextSecondaryWithCopy>{block?.extrinsicsRoot}</TextSecondaryWithCopy>
     ),
     Validator: <Address address={block?.validator} ellipsis={false} />,
+  };
+};
+
+export const toAccountDetailItem = (id, account, chainSetting) => {
+  return {
+    Address: <Address address={id} ellipsis={false} />,
+    "Total Balance": (
+      <Tooltip
+        tip={`${toPrecision(account?.data?.total, chainSetting.decimals)} ${
+          chainSetting.symbol
+        }`}
+      >
+        <TextSecondary>
+          <ValueDisplay
+            value={toPrecision(account?.data?.total, chainSetting.decimals)}
+            symbol={chainSetting.symbol}
+            abbreviate={false}
+          />
+        </TextSecondary>
+      </Tooltip>
+    ),
+    Free: (
+      <Tooltip
+        tip={`${toPrecision(account?.data?.free, chainSetting.decimals)} ${
+          chainSetting.symbol
+        }`}
+      >
+        <TextSecondary>
+          <ValueDisplay
+            value={toPrecision(account?.data?.free, chainSetting.decimals)}
+            symbol={chainSetting.symbol}
+            abbreviate={false}
+          />
+        </TextSecondary>
+      </Tooltip>
+    ),
+    Reversed: (
+      <Tooltip
+        tip={`${toPrecision(account?.data?.reversed, chainSetting.decimals)} ${
+          chainSetting.symbol
+        }`}
+      >
+        <TextSecondary>
+          <ValueDisplay
+            value={toPrecision(account?.data?.reserved, chainSetting.decimals)}
+            symbol={chainSetting.symbol}
+            abbreviate={false}
+          />
+        </TextSecondary>
+      </Tooltip>
+    ),
+    Nonce: <TextSecondary>{account?.nonce}</TextSecondary>,
+  };
+};
+
+export const toCallDetailItem = (indexer, section, method) => {
+  return {
+    "Call ID": (
+      <DetailedCallId
+        blockHeight={indexer?.blockHeight}
+        id={indexer?.callIndex}
+      />
+    ),
+    "Extrinsics ID": (
+      <DetailedExtrinsicId
+        blockHeight={indexer?.blockHeight}
+        id={indexer?.extrinsicIndex}
+      />
+    ),
+    Block: <DetailedBlock blockHeight={indexer?.blockHeight} />,
+    Timestamp: <DetailedTime ts={indexer?.blockTime} />,
+    Method: <Tag>{method}</Tag>,
+    Call: (
+      <CallText>
+        {section}({method})
+      </CallText>
+    ),
   };
 };
