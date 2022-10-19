c0a25eef34cecc687b6c3d6970136b5e7248fba6
yoshiyuki <brother-zhou@qq.com>
2022-10-19 14:41:16 +0800

    fix: pagination caret area (#200)

--- site/src/components/pagination/caret.js
+++ site/src/components/pagination/caret.js
@@ -7,7 +7,10 @@ import encodeUriQuery from "../../utils/viewFuncs";
 import { useLocation } from "react-router-dom";
 
 const StyledLink = styled(Link)`
-  height: 16px;
+  display: flex;
+  justify-content: center;
+  width: 28px;
+  height: 28px;
 `;
 
 const LinkInnerWrapper = styled.div`
@@ -29,14 +32,13 @@ export default function PageCaret({ children, page, onPageChange = null }) {
   }, [location, page]);
 
   return (
-    <StyledLink to={to}>
-      <LinkInnerWrapper
-        onClick={(e) => {
-          onPageChange && onPageChange(e, page);
-        }}
-      >
-        {children}
-      </LinkInnerWrapper>
+    <StyledLink
+      to={to}
+      onClick={(e) => {
+        onPageChange && onPageChange(e, page);
+      }}
+    >
+      <LinkInnerWrapper>{children}</LinkInnerWrapper>
     </StyledLink>
   );
 }
