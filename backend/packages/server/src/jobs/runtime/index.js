7a4576c18a8ebbf87b9f1aa329f42e3052f5f450
Yongfeng LI <wliyongfeng@gmail.com>
2022-10-20 11:31:25 +0800

    Normalize and save specs

--- backend/packages/server/src/jobs/runtime/index.js
+++ backend/packages/server/src/jobs/runtime/index.js
@@ -1,19 +1,55 @@
 const {
   runtime: { getRuntimeCollection },
 } = require("@statescan/mongo");
+const { stringCamelCase } = require("@polkadot/util");
 
-let specVersions = [];
+let specs = [];
+
+function normalizeTypeVariants(callsType, types) {
+  const type = types[callsType];
+  return type.type.def?.variant?.variants?.map((call) =>
+    stringCamelCase(call.name),
+  );
+}
+
+function constructOnePallet(pallet, types) {
+  const { name, calls, events } = pallet;
+  return {
+    name,
+    calls: calls ? normalizeTypeVariants(calls.type, types) : [],
+    events: events ? normalizeTypeVariants(events.type, types) : [],
+  };
+}
+
+function constructSpecFromMetadata(metadata = {}) {
+  const { lookup: { types } = {}, pallets = [] } = metadata;
+  return pallets.reduce((result, pallet) => {
+    result.push(constructOnePallet(pallet, types));
+    return result;
+  }, []);
+}
 
 async function queryAndSetSpecVersions() {
   const col = await getRuntimeCollection();
   const versions = await col.find({}, { projection: { height: 0 } }).toArray();
-  specVersions = versions.map((version) => version.runtimeVersion.specVersion);
+  specs = versions.map((version) => {
+    return {
+      specVersion: version.runtimeVersion.specVersion,
+      pallets: constructSpecFromMetadata(version.metadata),
+    };
+  });
 }
 
 async function queryAndSetSpecs() {
+  await queryAndSetSpecVersions();
   setInterval(queryAndSetSpecVersions, 6 * 1000);
+}
+
+function getSpecs() {
+  return specs;
 }
 
 module.exports = {
   queryAndSetSpecs,
+  getSpecs,
 };
