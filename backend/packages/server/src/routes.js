97951ba31611e1698c8e16d9ebbe0c00d8723cb8
Yongfeng LI <wliyongfeng@gmail.com>
2022-10-20 11:41:12 +0800

    Add specs filter api

--- backend/packages/server/src/routes.js
+++ backend/packages/server/src/routes.js
@@ -10,6 +10,7 @@ const routes = [
   require("./features/transfers/routes"),
   require("./features/unfinalized/routes"),
   require("./features/search/routes"),
+  require("./features/spec/routes"),
 ];
 
 module.exports = (app) => {
