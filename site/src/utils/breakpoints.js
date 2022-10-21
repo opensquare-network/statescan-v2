d31b80f7c848bb00947fd79d4c3b31350bfe9c9f
2nthony <ant.hu@outlook.com>
2022-10-21 20:15:15 +0800

    feat: add lg breakpoint

--- site/src/utils/breakpoints.js
+++ site/src/utils/breakpoints.js
@@ -4,6 +4,12 @@
  */
 export const mdcss = (css) => makeBreakpoint(css, 992);
 
+/**
+ * @param {import("styled-components").ThemedCssFunction} css
+ * @description less than 1200
+ */
+export const lgcss = (css) => makeBreakpoint(css, 1200);
+
 function makeBreakpoint(css, breakpoint) {
   return `@media (max-width: ${breakpoint}px) {
     ${css}
