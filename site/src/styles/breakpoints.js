503de84d789ec865bc99384ef277f2fcb5804d19
2nthony <ant.hu@outlook.com>
2022-10-24 11:17:29 +0800

    feat: export breakpoint size

--- site/src/styles/breakpoints.js
+++ site/src/styles/breakpoints.js
@@ -1,24 +1,28 @@
 // https://tailwindcss.com/docs/responsive-design
 // but use `max-width`
 
+export const SM_SIZE = 768;
+export const MD_SIZE = 1024;
+export const LG_SIZE = 1280;
+
 /**
  * @param {import("styled-components").ThemedCssFunction} css
  * @alias mobilecss
  * @description less than 768
  */
-export const smcss = (css) => makeBreakpoint(css, 768);
+export const smcss = (css) => makeBreakpoint(css, SM_SIZE);
 
 /**
  * @param {import("styled-components").ThemedCssFunction} css
  * @description less than 992
  */
-export const mdcss = (css) => makeBreakpoint(css, 1024);
+export const mdcss = (css) => makeBreakpoint(css, MD_SIZE);
 
 /**
  * @param {import("styled-components").ThemedCssFunction} css
  * @description less than 1200
  */
-export const lgcss = (css) => makeBreakpoint(css, 1280);
+export const lgcss = (css) => makeBreakpoint(css, LG_SIZE);
 
 function makeBreakpoint(css, breakpoint) {
   return `@media (max-width: ${breakpoint}px) {
