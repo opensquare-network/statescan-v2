c54a3590013737368643fbf4c85b0186f6d42898
2nthony <ant.hu@outlook.com>
2022-10-24 11:09:13 +0800

    fix: breakpoint now use tailwindcss design

--- site/src/styles/breakpoints.js
+++ site/src/styles/breakpoints.js
@@ -1,4 +1,4 @@
-// https://getbootstrap.com/docs/5.2/layout/breakpoints/
+// https://tailwindcss.com/docs/responsive-design
 // but use `max-width`
 
 /**
@@ -12,13 +12,13 @@ export const smcss = (css) => makeBreakpoint(css, 768);
  * @param {import("styled-components").ThemedCssFunction} css
  * @description less than 992
  */
-export const mdcss = (css) => makeBreakpoint(css, 992);
+export const mdcss = (css) => makeBreakpoint(css, 1024);
 
 /**
  * @param {import("styled-components").ThemedCssFunction} css
  * @description less than 1200
  */
-export const lgcss = (css) => makeBreakpoint(css, 1200);
+export const lgcss = (css) => makeBreakpoint(css, 1280);
 
 function makeBreakpoint(css, breakpoint) {
   return `@media (max-width: ${breakpoint}px) {
