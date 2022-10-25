599714bd9ed5783aa7c7718233257fea780fb043
Yongfeng LI <wliyongfeng@gmail.com>
2022-10-25 12:52:04 +0800

    Remove useless code, #170

--- backend/packages/asset-scan/src/scan/events/assets/destroyed.js
+++ backend/packages/asset-scan/src/scan/events/assets/destroyed.js
@@ -1,10 +1,6 @@
 const { flushAssetsData } = require("../flush");
 const { deleteAssetHolders } = require("../../../service/assets/holders");
 const { deleteAssetApprovals } = require("../../../service/assets/approval");
-const { saveAssets } = require("../../../service/assets/updateAssets");
-const {
-  batchInsertAssetsTransfers,
-} = require("../../../service/assets/batchInsertTransfers");
 const { insertAssetTimeline } = require("../../mongo/assets/insertTimeline");
 const { updateActiveAsset } = require("../../mongo/assets/updateAsset");
 
@@ -15,11 +11,9 @@ async function handleDestroyed(event, indexer) {
   const assetId = data[0].toNumber();
   await insertAssetTimeline(assetId, method, {}, indexer);
 
-  await batchInsertAssetsTransfers(indexer);
-  await saveAssets(indexer);
-
   await deleteAssetHolders(assetId, indexer);
   await deleteAssetApprovals(assetId, indexer);
+
   await updateActiveAsset(assetId, { destroyed: true });
 }
 
