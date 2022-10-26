d3a6ffbfd7163250620e2d8c59c255868ce1ad78
2nthony <ant.hu@outlook.com>
2022-10-26 18:01:32 +0800

    refactor: page extrinsics redux (#262)
    * chore: clean unused
    
    * feat: extrinsic list api
    
    * feat: extrinsic slice add list api
    
    * refactor: page extrinsics redux

--- site/src/store/reducers/extrinsicSlice.js
+++ site/src/store/reducers/extrinsicSlice.js
@@ -1,26 +1,53 @@
 import { createSlice } from "@reduxjs/toolkit";
 import api from "../../services/api";
-import { extrinsicApi } from "../../services/urls";
+import { extrinsicApi, extrinsicListApi } from "../../services/urls";
 
 const name = "extrinsic";
 
 const extrinsicSlice = createSlice({
   name,
   initialState: {
+    list: null,
+    listLoading: false,
     detail: null,
-    eventsTable: {},
-    callsTable: {},
   },
   reducers: {
+    setList(state, { payload }) {
+      state.list = payload;
+    },
+    setListLoading(state, { payload }) {
+      state.listLoading = payload;
+    },
     setDetail(state, { payload }) {
       state.detail = payload;
     },
   },
 });
 
-export const { setDetail } = extrinsicSlice.actions;
+export const { setDetail, setList, setListLoading } = extrinsicSlice.actions;
 
+export const extrinsicListSelector = (state) => state[name].list;
+export const extrinsicListLoadingSelector = (state) => state[name].listLoading;
 export const extrinsicDetailSelector = (state) => state[name].detail;
+
+export const extrinsicFetchList =
+  (page, pageSize, params, fetchOptions) => async (dispatch) => {
+    dispatch(setListLoading(true));
+
+    return api
+      .fetch(extrinsicListApi, { page, pageSize, ...params }, fetchOptions)
+      .then(({ result }) => {
+        if (result) {
+          dispatch(setList(result));
+        }
+      })
+      .finally(() => {
+        dispatch(setListLoading(false));
+      });
+  };
+export const cleanExtrinsicList = () => (dispatch) => {
+  dispatch(setList(null));
+};
 
 export const extrinsicFetchDetail = (id) => async (dispatch) => {
   return api.fetch(extrinsicApi(id)).then(({ result }) => {
