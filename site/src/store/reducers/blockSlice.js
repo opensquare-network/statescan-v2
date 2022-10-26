9bf0a8922949439d2d9a2274e182d26cb608521b
2nthony <ant.hu@outlook.com>
2022-10-26 18:34:47 +0800

    Refactor/page blocks redux (#263)
    * clean
    
    * refactor: page blocks redux

--- site/src/store/reducers/blockSlice.js
+++ site/src/store/reducers/blockSlice.js
@@ -1,26 +1,53 @@
 import { createSlice } from "@reduxjs/toolkit";
 import api from "../../services/api";
-import { blockApi } from "../../services/urls";
+import { blockApi, blockListApi } from "../../services/urls";
 
 const name = "block";
 
 const blockSlice = createSlice({
   name,
   initialState: {
+    list: null,
+    listLoading: false,
     detail: null,
-    eventsTable: {},
-    callsTable: {},
   },
   reducers: {
+    setList(state, { payload }) {
+      state.list = payload;
+    },
+    setListLoading(state, { payload }) {
+      state.listLoading = payload;
+    },
     setDetail(state, { payload }) {
       state.detail = payload;
     },
   },
 });
 
-export const { setDetail } = blockSlice.actions;
+export const { setDetail, setList, setListLoading } = blockSlice.actions;
 
+export const blockListSelector = (state) => state[name].list;
+export const blockListLoadingSelector = (state) => state[name].listLoading;
 export const blockDetailSelector = (state) => state[name].detail;
+
+export const blockFetchList =
+  (page, pageSize, params, fetchOptions) => async (dispatch) => {
+    dispatch(setListLoading(true));
+
+    return api
+      .fetch(blockListApi, { page, pageSize, ...params }, fetchOptions)
+      .then(({ result }) => {
+        if (result) {
+          dispatch(setList(result));
+        }
+      })
+      .finally(() => {
+        dispatch(setListLoading(false));
+      });
+  };
+export const cleanBlockList = () => (dispatch) => {
+  dispatch(setList(null));
+};
 
 export const blockFetchDetail = (id) => async (dispatch) => {
   return api.fetch(blockApi(id)).then(({ result }) => {
