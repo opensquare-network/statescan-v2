46068d920780fc2ac77d522d6b2c8a1fba5b7918
2nthony <ant.hu@outlook.com>
2022-10-27 17:05:30 +0800

    refactor: page calls redux

--- site/src/pages/calls.js
+++ site/src/pages/calls.js
@@ -1,46 +1,49 @@
 import { useEffect } from "react";
-import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import BreadCrumb from "../components/breadCrumb";
 import Layout from "../components/layout";
 import Pagination from "../components/pagination";
 import { StyledPanelTableWrapper } from "../components/styled/panel";
-import api from "../services/api";
 import { getPageFromQuery } from "../utils/viewFuncs";
 import CallsTable from "../components/call/callsTable";
 import { LIST_DEFAULT_PAGE_SIZE } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  callFetchList,
+  callListLoadingSelector,
+  callListSelector,
+} from "../store/reducers/callSlice";
 
 function Calls() {
   const location = useLocation();
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [total, setTotal] = useState(0);
+  const dispatch = useDispatch();
   const page = getPageFromQuery(location);
   const pageSize = LIST_DEFAULT_PAGE_SIZE;
 
+  const list = useSelector(callListSelector);
+  const loading = useSelector(callListLoadingSelector);
+
   useEffect(() => {
-    setLoading(true);
-    api
-      .fetch("/calls", {
-        page: page - 1,
-        pageSize,
-      })
-      .then(({ result }) => {
-        setData(result?.items ?? []);
-        setTotal(result?.total ?? 0);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, [location, page, pageSize]);
+    const controller = new AbortController();
+
+    dispatch(
+      callFetchList(page - 1, pageSize, null, { signal: controller.signal }),
+    );
+
+    return () => controller.abort();
+  }, [dispatch, page, pageSize]);
 
   return (
     <Layout>
       <BreadCrumb data={[{ name: "Calls" }]} />
 
       <StyledPanelTableWrapper>
-        <CallsTable data={data} loading={loading} />
-        <Pagination page={parseInt(page)} pageSize={pageSize} total={total} />
+        <CallsTable data={list?.items} loading={loading} />
+        <Pagination
+          page={parseInt(page)}
+          pageSize={pageSize}
+          total={list?.total}
+        />
       </StyledPanelTableWrapper>
     </Layout>
   );
