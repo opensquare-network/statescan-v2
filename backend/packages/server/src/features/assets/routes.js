ab3fb19895e52a7970d7d06e248705c48e576345
yoshiyuki <brother-zhou@qq.com>
2022-11-02 15:17:11 +0800

    feat: asset detail api (#294)
    * feat: asset detail api
    
    * clean up console.log
    
    * refactor: rename param to height

--- backend/packages/server/src/features/assets/routes.js
+++ backend/packages/server/src/features/assets/routes.js
@@ -1,7 +1,9 @@
 const Router = require("koa-router");
 const { getAssets } = require("./controllers/assets");
+const { getAsset } = require("./controllers/asset");
 
 const router = new Router();
 router.get("/assets", getAssets);
+router.get("/asset/:assetId(\\d+)_:height(\\d+)", getAsset);
 
 module.exports = router;
