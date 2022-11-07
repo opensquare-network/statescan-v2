ac210f2ccfd5a38a862a8443400956b2f093fc53
yoshiyuki <brother-zhou@qq.com>
2022-11-07 11:08:26 +0800

    fix: layout scroll issue, #309

--- site/src/components/layout/container.js
+++ site/src/components/layout/container.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   margin: 0 auto;
   width: 100%;
-  max-width: 1312px;
+  max-width: 1360px;
   padding: 0 24px;
   @media screen and (max-width: 600px) {
     padding: 0 16px;
   }
-  box-sizing: content-box;
+  box-sizing: border-box;
 `;
 
 export default function Container({ children, style = {} }) {
