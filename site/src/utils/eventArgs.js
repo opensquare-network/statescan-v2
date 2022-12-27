8a21c44b601bb7ef8ac24cd433c357d0dd265acc
chaojun <chaojun@opensquare.network>
2022-12-27 12:40:46 +0800

    Improve event field name display

--- site/src/utils/eventArgs.js
+++ site/src/utils/eventArgs.js
@@ -10,24 +10,18 @@ function cleanTemplateArgs(typeName) {
 }
 
 export function makeEventArgs(event) {
-  const eventData = {
-    object_type: "table_pairs",
-    object_data: event?.args?.map((item) => [
-      cleanTemplateArgs(item.typeName).split("::").pop(),
-      item.value,
-    ]),
-  };
-
   return {
-    object_type: eventData.object_type,
-    object_data: eventData.object_data.map(([type, val]) => {
-      if (type === "AccountId") {
+    object_type: "table_pairs",
+    object_data: event?.args?.map((item) => {
+      const fieldType = cleanTemplateArgs(item.typeName).split("::").pop();
+      if (fieldType === "AccountId") {
         return [
-          type,
-          <AddressOrIdentity key="0" address={val} ellipsis={false} />,
+          item.name,
+          <AddressOrIdentity key="0" address={item.value} ellipsis={false} />,
         ];
       }
-      return [type, val];
+
+      return [item.name, item.value];
     }),
   };
 }
