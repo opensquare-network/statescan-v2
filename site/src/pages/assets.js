111fbc66b33a0f8db5055da4505b023543cd217b
hyifeng <hyifeng@gmail.com>
2023-01-03 18:06:50 +0800

    Refs #353, add assets total supply tip (#358)

--- site/src/pages/assets.js
+++ site/src/pages/assets.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout";
 import Table from "../components/table";
 import Pagination from "../components/pagination";
 import { useLocation } from "react-router-dom";
-import { getPageFromQuery } from "../utils/viewFuncs";
+import { bigNumberToLocaleString, getPageFromQuery } from "../utils/viewFuncs";
 import { toPrecision } from "@osn/common";
 import ValueDisplay from "../components/displayValue";
 import { useDispatch, useSelector } from "react-redux";
@@ -22,6 +22,21 @@ import Tooltip from "../components/tooltip";
 import { Flex } from "../components/styled/flex";
 import SymbolName from "../components/symbol/name";
 import Symbol from "../components/symbol";
+import styled from "styled-components";
+import { Inter_12_600 } from "../styles/text";
+
+const TipTitle = styled.div`
+  ${Inter_12_600}
+`;
+
+function TotalSupplyTip({ supply }) {
+  return (
+    <div>
+      <TipTitle>Total Supply</TipTitle>
+      <div>{bigNumberToLocaleString(supply)}</div>
+    </div>
+  );
+}
 
 function Assets() {
   const location = useLocation();
@@ -50,6 +65,10 @@ function Assets() {
 
   const data =
     list?.items?.map((asset) => {
+      const supply = toPrecision(
+        asset?.detail?.supply,
+        asset?.metadata?.decimals,
+      );
       return [
         <ColoredInterLink to={`/asset/${asset.assetId}_${asset.assetHeight}`}>
           #{asset.assetId}
@@ -69,10 +88,9 @@ function Assets() {
           <AddressOrIdentity address={asset?.detail?.issuer} />
         </Tooltip>,
         asset?.detail?.accounts,
-        <ValueDisplay
-          value={toPrecision(asset?.detail?.supply, asset?.metadata?.decimals)}
-          symbol={asset.symbol}
-        />,
+        <Tooltip pullRight={true} tip={<TotalSupplyTip supply={supply} />}>
+          <ValueDisplay value={supply} symbol={asset.symbol} />
+        </Tooltip>,
       ];
     }) ?? null;
 
