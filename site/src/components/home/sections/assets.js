e17e8a250821e3731580851b29b422a6ce695321
2nthony <ant.hu@outlook.com>
2023-01-04 18:57:37 +0800

    refactor: assets table data from hook

--- site/src/components/home/sections/assets.js
+++ site/src/components/home/sections/assets.js
@@ -1,19 +1,12 @@
-import { toPrecision } from "@osn/common";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   assetFetchList,
   assetListLoadingSelector,
-  assetListSelector,
 } from "../../../store/reducers/assetSlice";
 import { assetsHead } from "../../../utils/constants";
-import AddressOrIdentity from "../../address";
-import ValueDisplay from "../../displayValue";
-import { ColoredInterLink } from "../../styled/link";
-import Symbol from "../../symbol";
-import SymbolName from "../../symbol/name";
+import { useAssetsTableData } from "../../../utils/hooks/useAssetsTableData";
 import Table from "../../table";
-import Tooltip from "../../tooltip";
 
 const page = 0;
 const pageSize = 4;
@@ -21,8 +14,8 @@ const pageSize = 4;
 export default function Assets() {
   const dispatch = useDispatch();
 
-  const assets = useSelector(assetListSelector);
   const loading = useSelector(assetListLoadingSelector);
+  const data = useAssetsTableData();
 
   useEffect(() => {
     const controller = new AbortController();
@@ -35,32 +28,6 @@ export default function Assets() {
 
     return () => controller.abort();
   }, [dispatch]);
-
-  const data =
-    assets?.items?.map((asset) => {
-      return [
-        <ColoredInterLink to={`/asset/${asset.assetId}_${asset.assetHeight}`}>
-          #{asset.assetId}
-        </ColoredInterLink>,
-        asset?.metadata?.symbol ? <Symbol asset={asset} /> : "--",
-        asset?.metadata?.name ? (
-          <SymbolName name={asset.metadata.name} />
-        ) : (
-          "--"
-        ),
-        <Tooltip tip={asset?.detail?.owner}>
-          <AddressOrIdentity address={asset?.detail?.owner} />
-        </Tooltip>,
-        <Tooltip tip={asset?.detail?.issuer}>
-          <AddressOrIdentity address={asset?.detail?.issuer} />
-        </Tooltip>,
-        asset?.detail?.accounts,
-        <ValueDisplay
-          value={toPrecision(asset?.detail?.supply, asset?.metadata?.decimals)}
-          symbol={asset.symbol}
-        />,
-      ];
-    }) ?? null;
 
   return <Table heads={assetsHead} data={data} loading={loading} />;
 }
