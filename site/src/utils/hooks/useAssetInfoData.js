fb1e2dfd18cc771cab8c9a6295e861acca342fc1
2nthony <ant.hu@outlook.com>
2023-01-06 11:51:14 +0800

    refactor!: use asset info data, add `useAssetInfoDataDetail` for detail page

--- site/src/utils/hooks/useAssetInfoData.js
+++ site/src/utils/hooks/useAssetInfoData.js
@@ -3,11 +3,23 @@ import { assetDetailSelector } from "../../store/reducers/assetSlice";
 import { chainSelector } from "../../store/reducers/settingSlice";
 import assetInfoData from "../consts/assetInfoData";
 
+/**
+ * @description return all asset info data
+ */
 export function useAssetInfoData() {
   const chain = useSelector(chainSelector);
-  const detail = useSelector(assetDetailSelector);
   const assetInfo = assetInfoData[chain] ?? {};
-  const data = assetInfo[detail?.assetId] ?? {};
+
+  return assetInfo;
+}
+
+/**
+ * @description use in asset detail page
+ */
+export function useAssetInfoDataDetail() {
+  const detail = useSelector(assetDetailSelector);
+  const assetInfoData = useAssetInfoData();
+  const data = assetInfoData[detail?.assetId] ?? {};
 
   return data;
 }
