12139f5ea9dd1f8956106c62bc97f40da093439a
Yongfeng LI <wliyongfeng@gmail.com>
2023-01-06 17:19:37 +0800

    Add handleMetadataResource to metadata handling, #351

--- backend/packages/uniques-scan/src/ipfs/metadata/index.js
+++ backend/packages/uniques-scan/src/ipfs/metadata/index.js
@@ -1,9 +1,11 @@
 const { handleMetadataDefinition } = require("./definition");
 const { handleMetadataCID } = require("./cid");
+const { handleMetadataResource } = require("./resource");
 
 async function handleMetadata() {
   await handleMetadataCID();
   await handleMetadataDefinition();
+  await handleMetadataResource();
 }
 
 module.exports = {
