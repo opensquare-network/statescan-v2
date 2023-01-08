81a2d105953e48dfc4ffa3f59394505d2d726f23
Yongfeng LI <wliyongfeng@gmail.com>
2023-01-08 17:34:48 +0800

    Add populate and definition parse to metadata handle, #351

--- backend/packages/uniques-scan/src/ipfs/metadata/index.js
+++ backend/packages/uniques-scan/src/ipfs/metadata/index.js
@@ -1,4 +1,10 @@
-async function handleMetadata() {}
+const { populateMetadata } = require("./populate");
+const { parseDefinition } = require("./definition");
+
+async function handleMetadata() {
+  await populateMetadata();
+  await parseDefinition();
+}
 
 module.exports = {
   handleMetadata,
