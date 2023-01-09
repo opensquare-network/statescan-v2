fe799f065b9a5abbf8a2c6c0fc376b791edac468
2nthony <ant.hu@outlook.com>
2023-01-09 14:53:44 +0800

    feat: asset analytics body

--- site/src/components/charts/analytics/body.js
+++ site/src/components/charts/analytics/body.js
@@ -1,6 +1,10 @@
-import styled from "styled-components";
+import "../config";
+import { Line } from "react-chartjs-2";
+import styled, { useTheme } from "styled-components";
 import { Inter_12_500 } from "../../../styles/text";
 import { FlexBetween, FlexColumn } from "../../styled/flex";
+import { abbreviateBigNumber } from "@osn/common";
+import { fromAssetUnit } from "../../../utils/index";
 
 const Wrapper = styled(FlexColumn)`
   padding: 24px 0;
@@ -11,7 +15,132 @@ const Label = styled.span`
   ${Inter_12_500};
 `;
 
-export default function AnalyticsChartBody() {
+const ChartWrapper = styled.div`
+  height: 240px;
+`;
+
+const datasetLineBase = {
+  fill: false,
+  tension: 0.3,
+  pointHoverBorderWidth: 1,
+  pointRadius: 0,
+  pointHitRadius: 10,
+  borderWidth: 2,
+};
+
+export default function AnalyticsChartBody({
+  data = [],
+  decimals,
+  amountHidden,
+  countHidden,
+  holdersHidden,
+}) {
+  const theme = useTheme();
+
+  // FIXME: `$numberDecimal` ?
+  const transferAmounts = data?.map((item) =>
+    fromAssetUnit(item.transferAmount?.$numberDecimal ?? 0, decimals),
+  );
+  const transferCounts = data?.map((item) => item.transferCount);
+  const holderCounts = data?.map((item) => item.holderCount);
+
+  const chartLabels = data?.map((item) => item?.indexer?.blockTime);
+  const chartDatasets = [
+    {
+      label: "Transfer Amount",
+      data: transferAmounts,
+      yAxisID: "y",
+      borderColor: theme.fillActiveBlue,
+      pointBackgroundColor: theme.fillActiveBlue,
+      hidden: amountHidden,
+      ...datasetLineBase,
+    },
+    {
+      label: "Transfer Count",
+      data: transferCounts,
+      yAxisID: "y1",
+      borderColor: "#52CC8A",
+      pointBackgroundColor: "#52CC8A",
+      hidden: countHidden,
+      ...datasetLineBase,
+    },
+    {
+      label: "Holders",
+      data: holderCounts,
+      yAxisID: "y1",
+      borderColor: "#1FABE8",
+      pointBackgroundColor: "#1FABE8",
+      hidden: holdersHidden,
+      ...datasetLineBase,
+    },
+  ];
+
+  const chartData = {
+    labels: chartLabels,
+    datasets: chartDatasets,
+  };
+  const chartOptions = {
+    type: "line",
+    clip: false,
+    animation: {
+      duration: 0,
+    },
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        id: "a",
+        position: "left",
+        type: "linear",
+        beginAtZero: true,
+        grid: {
+          display: false,
+        },
+        ticks: {
+          callback(value) {
+            return abbreviateBigNumber(value);
+          },
+        },
+        border: {
+          display: false,
+        },
+      },
+      y1: {
+        id: "b",
+        position: "right",
+        type: "linear",
+        beginAtZero: true,
+        grid: {
+          drawTicks: false,
+        },
+        border: {
+          display: false,
+        },
+      },
+      x: {
+        type: "time",
+        time: {
+          displayFormats: {
+            day: "MMM, DD",
+          },
+          unit: "day",
+          unitStepSize: 2,
+        },
+        grid: {
+          display: false,
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        mode: "index",
+        intersect: false,
+      },
+    },
+  };
+
   return (
     <Wrapper gap={16}>
       <FlexBetween>
@@ -19,7 +148,9 @@ export default function AnalyticsChartBody() {
         <Label>Transfer Counts</Label>
       </FlexBetween>
 
-      <div>TODO</div>
+      <ChartWrapper>
+        <Line data={chartData} options={chartOptions} />
+      </ChartWrapper>
     </Wrapper>
   );
 }
--- site/src/components/charts/analytics/index.js
+++ site/src/components/charts/analytics/index.js
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import moment from "moment";
+import { useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import api from "../../../services/api";
 import { assetDetailSelector } from "../../../store/reducers/assetSlice";
 import { ASSET_ANALYTICS_RANGE } from "../../../utils/constants";
 import { Panel } from "../../styled/panel";
@@ -12,12 +14,40 @@ const Wrapper = styled(Panel)`
   padding: 8px 24px;
 `;
 
-export default function AnalyticsChart() {
+export default function AnalyticsChart({ url }) {
   const [range, setRange] = useState(ASSET_ANALYTICS_RANGE.ALL);
   const detail = useSelector(assetDetailSelector);
   const [amountHidden, setAmountHidden] = useState(false);
   const [countHidden, setCountHidden] = useState(false);
   const [holdersHidden, setHoldersHidden] = useState(false);
+
+  const [assetsData, setAssetsData] = useState([]);
+
+  const rangeData = useMemo(() => {
+    const rangeDateMap = {
+      [ASSET_ANALYTICS_RANGE.ONE_MONTH]: "months",
+      [ASSET_ANALYTICS_RANGE.ONE_YEAR]: "years",
+    };
+
+    if (range === ASSET_ANALYTICS_RANGE.ALL) {
+      return assetsData;
+    } else {
+      const ts = moment().subtract(1, rangeDateMap[range]).valueOf();
+      return assetsData.filter((item) => item?.indexer?.blockTime > ts);
+    }
+  }, [range, assetsData]);
+
+  useEffect(() => {
+    if (!url) {
+      return;
+    }
+
+    api.fetch(url).then(({ result }) => {
+      if (result) {
+        setAssetsData(result);
+      }
+    });
+  }, [url]);
 
   return (
     <Wrapper>
@@ -28,7 +58,13 @@ export default function AnalyticsChart() {
         setRange={setRange}
       />
 
-      <AnalyticsChartBody />
+      <AnalyticsChartBody
+        data={rangeData}
+        decimals={detail?.metadata?.decimals}
+        amountHidden={amountHidden}
+        countHidden={countHidden}
+        holdersHidden={holdersHidden}
+      />
 
       <AnalyticsChartFooter
         amountHidden={amountHidden}
