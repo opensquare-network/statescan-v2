9d9c1db13e4ce4d9d1eae25ea0295641611adead
Yongfeng LI <wliyongfeng@gmail.com>
2023-01-09 15:35:08 +0800

    Improve active asset router

--- backend/packages/server/src/features/assets/controllers/asset.js
+++ backend/packages/server/src/features/assets/controllers/asset.js
@@ -2,15 +2,21 @@ const { HttpError } = require("../../../utils/httpError");
 const {
   asset: { getAssetCol },
 } = require("@statescan/mongo");
+const isNil = require("lodash.isnil");
 
 async function getAsset(ctx) {
   const { height, assetId } = ctx.params;
-  const col = await getAssetCol();
-  const q = {
-    assetHeight: parseInt(height),
+  let q = {
     assetId: parseInt(assetId),
   };
 
+  if (isNil(height)) {
+    q = { ...q, destroyed: false };
+  } else {
+    q = { ...q, assetHeight: parseInt(height) };
+  }
+
+  const col = await getAssetCol();
   const asset = await col.findOne(q, { projection: { _id: 0 } });
   if (!asset) {
     throw new HttpError(404, "asset not found");
