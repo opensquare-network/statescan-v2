0a71a5641b7026fbd907bf93453c5271da3cba7a
chaojun <chaojun@opensquare.network>
2023-01-11 12:33:01 +0800

    Refs #419, add account assets api

--- backend/packages/server/src/features/accounts/routes.js
+++ backend/packages/server/src/features/accounts/routes.js
@@ -3,6 +3,7 @@ const { getAccountTransfers } = require("./controllers/transfers");
 const { getAccountExtrinsics } = require("./controllers/extrinsics");
 const { getAccount } = require("./controllers/account");
 const { getAccounts } = require("./controllers/accounts");
+const { getAccountAssets } = require("./controllers/assets");
 
 const router = new Router();
 router.get("/accounts", getAccounts);
@@ -10,5 +11,6 @@ router.get("/accounts/:address", getAccount);
 
 router.get("/accounts/:address/transfers", getAccountTransfers);
 router.get("/accounts/:address/extrinsics", getAccountExtrinsics);
+router.get("/accounts/:address/assets", getAccountAssets);
 
 module.exports = router;
