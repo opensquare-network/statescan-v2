891a908cf49be9dc7567da5dbb3e80b67bf99d13
2nthony <ant.hu@outlook.com>
2023-01-13 14:57:49 +0800

    fix: eslint warnings

--- site/src/utils/hooks/useScrollLock.js
+++ site/src/utils/hooks/useScrollLock.js
@@ -1,29 +1,30 @@
+import { useRef } from "react";
+import { useCallback } from "react";
 import { useEffect, useState } from "react";
 
 export function useScrollLock(element = document.body) {
   const [isLocked, setIsLocked] = useState(false);
+  const initialOverflow = useRef();
 
-  let initialOverflow;
-
-  function lock() {
+  const lock = useCallback(() => {
     if (element) {
-      initialOverflow = element.style.overflow;
+      initialOverflow.current = element.style.overflow;
 
       if (isLocked) {
         element.style.overflow = "hidden";
       }
     }
-  }
+  }, [isLocked, element]);
 
-  function unlock() {
-    element.style.overflow = initialOverflow;
-  }
+  const unlock = useCallback(() => {
+    element.style.overflow = initialOverflow.current;
+  }, [element]);
 
   useEffect(() => {
     lock();
 
     return unlock;
-  }, [isLocked]);
+  }, [isLocked, lock, unlock]);
 
   return [isLocked, setIsLocked];
 }
