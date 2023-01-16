9699d9e4a84619e6e4b7260596a8bbdf4f7ddf7b
2nthony <ant.hu@outlook.com>
2023-01-16 15:31:50 +0800

    fix: address identity icon align

--- site/src/components/address/identity.js
+++ site/src/components/address/identity.js
@@ -1,20 +1,20 @@
 import styled from "styled-components";
 import IdentityIcon from "./identityIcon";
 import { Overpass_Mono_14_500 } from "../../styles/text";
+import { relative } from "../../styles/tailwindcss";
 
 const Wrapper = styled.div`
   display: inline-flex;
   align-items: center;
   position: relative;
-
-  svg {
-    margin-right: 4px;
-  }
 `;
 
 const IdentityIconWrapper = styled.span`
   display: inline-flex;
   align-items: center;
+  margin-right: 4px;
+  ${relative};
+  top: -1px;
 `;
 
 const Display = styled.span`
