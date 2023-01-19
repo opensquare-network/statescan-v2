4ea9ceb202360a35e636da0cd398af67502a48b6
2nthony <ant.hu@outlook.com>
2023-01-19 16:43:13 +0800

    fix: table time header ui

--- site/src/components/table/head/timeHead.js
+++ site/src/components/table/head/timeHead.js
@@ -1,12 +1,12 @@
 import { timeTypes } from "../../../utils/constants";
 import { Flex } from "../../styled/flex";
 import styled from "styled-components";
+import { Inter_12_600 } from "../../../styles/text";
 
 const Wrapper = styled(Flex)`
-  font-size: 14px;
-  line-height: 16px;
   color: ${(p) => p.theme.theme500};
   user-select: none;
+  ${Inter_12_600};
 `;
 
 const Item = styled.div`
@@ -17,7 +17,7 @@ export default function TimeHead({ timeType, setTimeType }) {
   return (
     <Wrapper>
       {timeType === timeTypes.date ? (
-        <Item onClick={() => setTimeType(timeTypes.age)}>Date Time</Item>
+        <Item onClick={() => setTimeType(timeTypes.age)}>Time</Item>
       ) : (
         <Item onClick={() => setTimeType(timeTypes.date)}>Age</Item>
       )}
