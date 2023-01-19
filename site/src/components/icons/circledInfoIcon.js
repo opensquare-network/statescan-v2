e98ca04741786ecb12b0c5997da92b681c6646d8
2nthony <ant.hu@outlook.com>
2023-01-19 17:02:22 +0800

    fix: overview tip icon use themed circled info icon

---
+++ site/src/components/icons/circledInfoIcon.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { ReactComponent as CircledInfo } from "./circled-info.svg";
+
+const CircledInfoIcon = styled(CircledInfo)`
+  & > path {
+    stroke-opacity: 1;
+    stroke: ${(p) => p.theme.fontTertiary};
+  }
+`;
+
+export default CircledInfoIcon;
