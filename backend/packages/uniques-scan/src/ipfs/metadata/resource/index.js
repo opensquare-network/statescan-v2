d9b6bbd836df3821b380cad5a12cc7f838fc1702
chaojun <chaojun@opensquare.network>
2023-01-19 12:42:30 +0800

    Fix: metadata parse script

--- backend/packages/uniques-scan/src/ipfs/metadata/resource/index.js
+++ backend/packages/uniques-scan/src/ipfs/metadata/resource/index.js
@@ -1,13 +1,28 @@
+const uniqBy = require("lodash.uniqby");
 const parseDataURL = require("data-urls");
 const {
-  uniques: { getMetadataCol },
+  uniques: { getMetadataCol, getResourceCol },
 } = require("@statescan/mongo");
 const { isCid } = require("../../utils/isCid");
 const { createThumbnailFromIpfsImage } = require("./ipfs");
 const { createThumbnailFromDataUrl } = require("./dataUrl");
+const { markMetadataResourceProcessed } = require("./store");
+
+async function checkResourceExistence(hash) {
+  const col = await getResourceCol();
+  const exists = await col.findOne({ hash });
+  return exists !== null;
+}
 
 async function parseOneResource(imageHash, image) {
   if (!imageHash || !image) {
+    return;
+  }
+
+  // Check if resource exists already
+  const isExistsAlready = await checkResourceExistence(imageHash);
+  if (isExistsAlready) {
+    await markMetadataResourceProcessed(imageHash);
     return;
   }
 
@@ -27,13 +42,17 @@ async function parseOneResource(imageHash, image) {
  */
 async function parseResource() {
   const col = await getMetadataCol();
-  let items = await col
+  const items = await col
     .find({ definitionValid: true, resourceProcessed: { $ne: true } })
     .toArray();
+
+  const images = uniqBy(
+    items.map((item) => item.definition),
+    (v) => v.imageHash,
+  );
+
   await Promise.all(
-    items.map((item) =>
-      parseOneResource(item.definition.imageHash, item.definition.image),
-    ),
+    images.map((item) => parseOneResource(item.imageHash, item.image)),
   );
 }
 
