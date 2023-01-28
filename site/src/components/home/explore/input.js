42920b90ff302678e05772e609be47a2fe193a3d
2nthony <ant.hu@outlook.com>
2023-01-28 14:47:21 +0800

    fix: explore input key arrow navigate

--- site/src/components/home/explore/input.js
+++ site/src/components/home/explore/input.js
@@ -5,6 +5,7 @@ import {
   useEffect,
   useImperativeHandle,
   useMemo,
+  useRef,
   useState,
 } from "react";
 import api from "../../../services/api";
@@ -34,23 +35,16 @@ function ExploreInput(props, ref) {
   const [term, setTerm] = useState("");
   const [hintsCache, setHintsCache] = useState({});
   const [dropdownVisible, setDropdownVisible] = useState(false);
-  const [selectedIndex, setSelectedIndex] = useState(0);
   const [loadingHints, setLoadingHints] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const dropdownRef = useRef();
 
   const hints = useMemo(() => hintsCache[term] ?? [], [term, hintsCache]);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const selectedItem = useMemo(() => {
-    if (dropdownVisible) {
-      return hints[selectedIndex];
-    }
-
-    return hints[0];
-  }, [selectedIndex, hints, dropdownVisible]);
-
-  // FIXME: see https://github.com/opensquare-network/statescan-v2/issues/196
   async function fetchHints(term) {
     setLoadingHints(true);
     return api
@@ -120,14 +114,9 @@ function ExploreInput(props, ref) {
 
     if (code === "Enter") {
       handleExplore();
-    } else if (code === "ArrowUp") {
-      if (selectedIndex > 0) {
-        setSelectedIndex(selectedIndex - 1);
-      }
-    } else if (code === "ArrowDown") {
-      if (selectedIndex < hints.length - 1) {
-        setSelectedIndex(selectedIndex + 1);
-      }
+    } else if (code === "ArrowUp" || code === "ArrowDown") {
+      event.preventDefault();
+      dropdownRef.current.handleArrowNavigate(code);
     }
   }
 
@@ -145,9 +134,11 @@ function ExploreInput(props, ref) {
       />
 
       <ExploreDropdown
+        ref={dropdownRef}
         hints={hints}
         visible={dropdownVisible}
-        selectedIndex={selectedIndex}
+        setSelectedItem={setSelectedItem}
+        onInputKeyDown={onInputKeyDown}
       />
     </>
   );
