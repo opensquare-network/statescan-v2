91ccdeabb83754084a3ef2eaf52a77a7939e82c5
Yongfeng LI <wliyongfeng@gmail.com>
2023-01-29 17:13:35 +0800

    Separate populate and parse scripts

--- backend/packages/uniques-scan/src/scripts/jobs/parse.js
+++ backend/packages/uniques-scan/src/scripts/jobs/parse.js
@@ -2,24 +2,22 @@ require("dotenv").config();
 
 const { oneMinute } = require("./common");
 const { parseDefinition } = require("../../ipfs/metadata/definition");
-const { populateMetadata } = require("../../ipfs/metadata/populate");
 const {
   uniques: { initUniquesScanDb },
 } = require("@statescan/mongo");
 
-async function populateAndParseMetadata() {
+async function parseMetadata() {
   try {
-    await populateMetadata();
     await parseDefinition();
   } finally {
-    setTimeout(populateAndParseMetadata, oneMinute * 3);
+    setTimeout(parseMetadata, oneMinute * 3);
   }
 }
 
 async function main() {
   // start all the jobs
   await initUniquesScanDb();
-  await populateAndParseMetadata();
+  await parseMetadata();
 }
 
 main().then(console.log);
