db451ae85a405bc879d49e1342b55afe67842f08
Yongfeng LI <wliyongfeng@gmail.com>
2023-01-29 14:48:30 +0800

    Increase metadata json fetch timeout

--- backend/packages/uniques-scan/src/ipfs/axios/json.js
+++ backend/packages/uniques-scan/src/ipfs/axios/json.js
@@ -7,7 +7,7 @@ async function fetchJson(cid) {
   for (const endpoint of endpoints) {
     const promise = axios
       .get(`${endpoint}${cid}`, {
-        timeout: 20 * 1000, // max 20 secs for fetching json data
+        timeout: 60 * 1000, // max 60 secs for fetching json data
       })
       .then((res) => res.data);
     promises.push(promise);
