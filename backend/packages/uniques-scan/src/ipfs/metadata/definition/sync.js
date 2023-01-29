afa15ec1ef0d01d34505116e558dd8b20e1cf5fe
Yongfeng LI <wliyongfeng@gmail.com>
2023-01-29 13:34:32 +0800

    Fix update

--- backend/packages/uniques-scan/src/ipfs/metadata/definition/sync.js
+++ backend/packages/uniques-scan/src/ipfs/metadata/definition/sync.js
@@ -8,7 +8,7 @@ async function syncOneMetadataValidity(col, hash, valid) {
     updates = { $set: { definitionValid: valid } };
   }
 
-  await col.updateMany({ dataHash: hash }, { updates });
+  await col.updateMany({ dataHash: hash }, updates);
 }
 
 module.exports = {
