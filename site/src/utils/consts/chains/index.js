0d3e8890f36b7bb09a569e18b5076015e876527a
Yongfeng LI <wliyongfeng@gmail.com>
2023-02-09 18:30:27 +0800

    Enable litmus

--- site/src/utils/consts/chains/index.js
+++ site/src/utils/consts/chains/index.js
@@ -2,11 +2,12 @@ import litentry from "./litentry";
 import statemine from "./statemine";
 import statemint from "./statemint";
 import westmint from "./westmint";
+import litmus from "./litmus";
 
 const chains = {
   // polkadot,
   litentry,
-  // litmus,
+  litmus,
   statemine,
   statemint,
   westmint,
