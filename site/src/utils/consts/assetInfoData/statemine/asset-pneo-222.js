ada8c101758173cc1ed8d4038cd3bc73abc048af
Adam Steeber <adam.steeber@gmail.com>
2023-02-27 22:54:17 -0600

    Add PNEO asset data, remove DANGER asset data
    I am helping the Planet Neo team launch their token - the Opensquare team can confirm by reaching out to Planet Neo. We are using the RMRK DAO app to handle project governance.
    
    I was also the one who created the DANGER token which served its purpose and was removed from the chain. Someone else took asset id 43 so the DANGER asset data should no longer be on that asset page

---
+++ site/src/utils/consts/assetInfoData/statemine/asset-pneo-222.js
@@ -0,0 +1,31 @@
+export const assetPNEO = {
+  id: 222,
+  data: {
+    icon: "/imgs/icons/asset/planetneo.png",
+    about: "Planet Neo is a collection of imaginary worlds designed to make the real world a better place. Get moving and solve global challenges with games, world building, and real-life experiences. We build brands with purpose to make life better.",
+    links: [
+      {
+        name: "Website",
+        url: "https://www.planetneo.com",
+      },
+      {
+        name: "Twitter: @planetneo_com",
+        url: "https://twitter.com/planetneo_com",
+        icon: "/imgs/icons/link-twitter.svg",
+      },
+      {
+        name: "Discord",
+        url: "https://discord.com/invite/3NKZxX7QPq",
+        icon: "/imgs/icons/link-discord.svg",
+      },
+    ],
+    governances: [
+      {
+        spaceLogo: "/imgs/icons/asset/rmrkdao.png",
+        spaceName: "NFT Polling",
+        projectName: "RMRK DAO",
+        link: "https://rmrkdao.app",
+      },
+    ],
+  },
+};
--- site/src/utils/consts/assetInfoData/statemine/index.js
+++ site/src/utils/consts/assetInfoData/statemine/index.js
@@ -1,17 +1,17 @@
 import { assetBfkk } from "./asset-bfkk-20";
 import { assetBill } from "./asset-bill-223";
 import { assetChrawnna } from "./asset-chrawnna-567";
-import { assetDanger } from "./asset-danger-43";
 import { assetPolarisDAO } from "./asset-polarisdao-16";
 import { assetRMRK } from "./asset-rmrk-8";
 import { assetUSDt } from "./asset-usdt-1984";
+import { assetPNEO } from "./asset-pneo-222";
 
 export const statemineAssetInfo = {
   [assetBfkk.id]: assetBfkk.data,
   [assetRMRK.id]: assetRMRK.data,
   [assetBill.id]: assetBill.data,
   [assetChrawnna.id]: assetChrawnna.data,
-  [assetDanger.id]: assetDanger.data,
   [assetPolarisDAO.id]: assetPolarisDAO.data,
   [assetUSDt.id]: assetUSDt.data,
+  [assetPNEO.id]: assetPNEO.data,
 };
