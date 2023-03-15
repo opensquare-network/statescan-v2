b0ae8bb306329e633f63e12396d6dfb9d9cd94df
2nthony <ant.hu@outlook.com>
2023-03-15 18:40:09 +0800

    init page runtime

--- site/src/pages/runtime.js
+++ site/src/pages/runtime.js
@@ -1,3 +1,53 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import BreadCrumb from "../components/breadCrumb";
+import DetailLayout from "../components/layout/detailLayout";
+import { Panel } from "../components/styled/panel";
+import {
+  clearRuntimeDetail,
+  runtimeDetailSelector,
+  runtimeFetchDetail,
+} from "../store/reducers/runtimeSlice";
+import {
+  clearHttpError,
+  handleApiError,
+} from "../utils/viewFuncs/errorHandles";
+
 export default function Runtime() {
-  return <div>page runtime</div>;
+  const { runtimeSlug } = useParams();
+  const [version, startHeight] = runtimeSlug.split("_");
+
+  const dispatch = useDispatch();
+  const runtime = useSelector(runtimeDetailSelector);
+
+  useEffect(() => {
+    if (version && startHeight) {
+      clearHttpError(dispatch);
+      dispatch(runtimeFetchDetail(version, startHeight)).catch((e) =>
+        handleApiError(e, dispatch),
+      );
+    }
+
+    return () => {
+      dispatch(clearRuntimeDetail());
+    };
+  }, [version, dispatch, startHeight]);
+
+  const breadCrumb = (
+    <BreadCrumb
+      data={[
+        { name: "Runtimes", path: "/runtimes" },
+        {
+          name: runtime ? version : "...",
+        },
+      ]}
+    />
+  );
+
+  return (
+    <DetailLayout breadCrumb={breadCrumb}>
+      <Panel></Panel>
+    </DetailLayout>
+  );
 }
