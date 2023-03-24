3fe2f28d932b3061d2e27bbb3048e71689d4909e
chaojun <chaojun@opensquare.network>
2023-03-24 11:44:53 +0800

    Remove debug code

--- site/src/hooks/useAchainableProfile.js
+++ site/src/hooks/useAchainableProfile.js
@@ -5,13 +5,11 @@ import api from "../services/api";
 export default function useAchainableProfile(address) {
   const [profile, setProfile] = useState(null);
   useEffect(() => {
-    api
-      .fetch(`http://127.0.0.1:5010${achainableProfileApi(address)}`)
-      .then(({ result }) => {
-        if (result) {
-          setProfile(result);
-        }
-      });
+    api.fetch(achainableProfileApi(address)).then(({ result }) => {
+      if (result) {
+        setProfile(result);
+      }
+    });
   }, [address]);
 
   return profile;
