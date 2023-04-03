852d1b423a8ea7b1e4da62f065465412fb2e7fab
2nthony <ant.hu@outlook.com>
2023-04-03 17:41:43 +0800

    typo

--- site/src/components/extrinsicAssetsTransferredList.js
+++ site/src/components/extrinsicAssetsTransferredList.js
@@ -179,7 +179,7 @@ export default function ExtrinsicAssetsTransferredList({
             />
           )}
           <ToggleListButton onClick={() => setExtraListVisible((v) => !v)}>
-            {extraListVisible ? "Hide" : "View All"}
+            {extraListVisible ? "Hide" : "View all"}
           </ToggleListButton>
         </>
       )}
