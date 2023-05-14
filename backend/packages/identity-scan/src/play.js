84bf5eda0a7ef277c7027167c3ed8d118931ce0c
Anwesh <anweshknayak@gmail.com>
2023-05-15 02:18:21 +0530

    fix timestamp issue and added test file play.js

---
+++ backend/packages/identity-scan/src/play.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const {
+    chain: { getApi, setSpecHeights, subscribeFinalizedHeight },
+} = require("@osn/scan-common");
+const { handleBlock } = require("./scan/block");
+const {
+    identity: { initIdentityScanDb },
+} = require("@statescan/mongo");
+
+async function main() {
+    await initIdentityScanDb();
+    await subscribeFinalizedHeight();
+    const blockHeights = [17740803];
+
+    const api = await getApi();
+    for (const height of blockHeights) {
+        await setSpecHeights([height - 1]);
+
+        const blockHash = await api.rpc.chain.getBlockHash(height);
+        const block = await api.rpc.chain.getBlock(blockHash);
+        const allEvents = await api.query.system.events.at(blockHash);
+
+        await handleBlock({
+            height,
+            block: block.block,
+            events: allEvents,
+        });
+        console.log(`${height} finished`);
+    }
+
+    console.log("finished");
+    process.exit(0);
+}
+
+main().then(console.log);
