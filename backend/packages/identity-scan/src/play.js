4b5378f05ba240f6e15da80c7e1f2e212c0d43f9
Anwesh <anweshknayak@gmail.com>
2023-05-15 05:42:12 +0530

    subIdentityOperations.js override display name

--- backend/packages/identity-scan/src/play.js
+++ backend/packages/identity-scan/src/play.js
@@ -11,7 +11,7 @@ async function main() {
     await initIdentityScanDb();
     await subscribeFinalizedHeight();
     // blockHeights for RegistarsTimelineCollection, identityCollection, subIdentityCollection
-    const blockHeights = [17740803,17884357,17664915];
+    const blockHeights = [17914288,17884357,17664915];
 
     const api = await getApi();
     for (const height of blockHeights) {
