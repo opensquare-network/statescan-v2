bd7b4cfaf77ab96af8f53921937ef7d2d37359de
Anwesh <anweshknayak@gmail.com>
2023-05-24 01:47:47 +0530

    fix(rc)

--- backend/packages/identity-scan/src/scan/events/identity/constants.js
+++ backend/packages/identity-scan/src/scan/events/identity/constants.js
@@ -1,14 +1,16 @@
-// constants.js
 module.exports = {
-  IDENTITY: "identity",
-  IDENTITY_SET: "IdentitySet",
-  IDENTITY_CLEARED: "IdentityCleared",
-  IDENTITY_KILLED: "IdentityKilled",
-  JUDGEMENT_GIVEN: "JudgementGiven",
-  JUDGEMENT_REQUESTED: "JudgementRequested",
-  JUDGEMENT_UNREQUESTED: "JudgementUnrequested",
-  REGISTRAR_ADDED: "RegistrarAdded",
-  SUB_IDENTITY_ADDED: "SubIdentityAdded",
-  SUB_IDENTITY_REMOVED: "SubIdentityRemoved",
-  SUB_IDENTITY_REVOKED: "SubIdentityRevoked",
+  SECTION: {
+    IDENTITY: "identity",
+  },
+  METHOD: {
+    IDENTITY_SET: "IdentitySet",
+    IDENTITY_CLEARED: "IdentityCleared",
+    IDENTITY_KILLED: "IdentityKilled",
+    JUDGEMENT_GIVEN: "JudgementGiven",
+    JUDGEMENT_REQUESTED: "JudgementRequested",
+    JUDGEMENT_UNREQUESTED: "JudgementUnrequested",
+    SUB_IDENTITY_ADDED: "SubIdentityAdded",
+    SUB_IDENTITY_REMOVED: "SubIdentityRemoved",
+    SUB_IDENTITY_REVOKED: "SubIdentityRevoked",
+  },
 };
--- backend/packages/identity-scan/src/scan/events/identity/index.js
+++ backend/packages/identity-scan/src/scan/events/identity/index.js
@@ -2,16 +2,18 @@ const { setIdentity, deleteIdentity } = require("./identityOperations");
 const { setRegistrarJudgement } = require("./registrarOperations");
 
 const {
-  IDENTITY_SET,
-  IDENTITY_CLEARED,
-  IDENTITY_KILLED,
-  JUDGEMENT_GIVEN,
-  JUDGEMENT_REQUESTED,
-  JUDGEMENT_UNREQUESTED,
-  SUB_IDENTITY_ADDED,
-  SUB_IDENTITY_REMOVED,
-  SUB_IDENTITY_REVOKED,
-  IDENTITY,
+  SECTION: { IDENTITY },
+  METHOD: {
+    IDENTITY_SET,
+    IDENTITY_CLEARED,
+    IDENTITY_KILLED,
+    JUDGEMENT_GIVEN,
+    JUDGEMENT_REQUESTED,
+    JUDGEMENT_UNREQUESTED,
+    SUB_IDENTITY_ADDED,
+    SUB_IDENTITY_REMOVED,
+    SUB_IDENTITY_REVOKED,
+  },
 } = require("./constants");
 const {
   setSubIdentity,
@@ -34,36 +36,33 @@ async function handleIdentityEvents(event, indexer) {
   if (IDENTITY !== section) {
     return;
   }
-
-  if (IDENTITY === section) {
-    if (IDENTITY_SET === method) {
-      await setIdentity(event);
-      await setIdentityEventForTimeline(IDENTITY_SET, event, indexer);
-    } else if (IDENTITY_CLEARED === method) {
-      await deleteIdentity(event);
-      await setIdentityEventForTimeline(IDENTITY_CLEARED, event, indexer);
-    } else if (IDENTITY_KILLED === method) {
-      await deleteIdentity(event);
-      await setIdentityEventForTimeline(IDENTITY_KILLED, event, indexer);
-    } else if (JUDGEMENT_GIVEN === method) {
-      await setRegistrarJudgement(JUDGEMENT_GIVEN, event, indexer);
-      await setIdentityEventForTimeline(JUDGEMENT_GIVEN, event, indexer);
-    } else if (JUDGEMENT_REQUESTED === method) {
-      await setRegistrarJudgement(JUDGEMENT_REQUESTED, event, indexer);
-      await setIdentityEventForTimeline(JUDGEMENT_REQUESTED, event, indexer);
-    } else if (JUDGEMENT_UNREQUESTED === method) {
-      await setRegistrarJudgement(JUDGEMENT_UNREQUESTED, event, indexer);
-      await setIdentityEventForTimeline(JUDGEMENT_UNREQUESTED, event, indexer);
-    } else if (SUB_IDENTITY_ADDED === method) {
-      await setSubIdentity(SUB_IDENTITY_ADDED, event, indexer);
-      await setIdentityEventForTimeline(SUB_IDENTITY_ADDED, event, indexer);
-    } else if (SUB_IDENTITY_REMOVED === method) {
-      await deleteSubIdentity(event);
-      await setIdentityEventForTimeline(SUB_IDENTITY_REMOVED, event, indexer);
-    } else if (SUB_IDENTITY_REVOKED === method) {
-      await deleteSubIdentity(event);
-      await setIdentityEventForTimeline(SUB_IDENTITY_REVOKED, event, indexer);
-    }
+  if (IDENTITY_SET === method) {
+    await setIdentity(event);
+    await setIdentityEventForTimeline(IDENTITY_SET, event, indexer);
+  } else if (IDENTITY_CLEARED === method) {
+    await deleteIdentity(event);
+    await setIdentityEventForTimeline(IDENTITY_CLEARED, event, indexer);
+  } else if (IDENTITY_KILLED === method) {
+    await deleteIdentity(event);
+    await setIdentityEventForTimeline(IDENTITY_KILLED, event, indexer);
+  } else if (JUDGEMENT_GIVEN === method) {
+    await setRegistrarJudgement(JUDGEMENT_GIVEN, event, indexer);
+    await setIdentityEventForTimeline(JUDGEMENT_GIVEN, event, indexer);
+  } else if (JUDGEMENT_REQUESTED === method) {
+    await setRegistrarJudgement(JUDGEMENT_REQUESTED, event, indexer);
+    await setIdentityEventForTimeline(JUDGEMENT_REQUESTED, event, indexer);
+  } else if (JUDGEMENT_UNREQUESTED === method) {
+    await setRegistrarJudgement(JUDGEMENT_UNREQUESTED, event, indexer);
+    await setIdentityEventForTimeline(JUDGEMENT_UNREQUESTED, event, indexer);
+  } else if (SUB_IDENTITY_ADDED === method) {
+    await setSubIdentity(SUB_IDENTITY_ADDED, event, indexer);
+    await setIdentityEventForTimeline(SUB_IDENTITY_ADDED, event, indexer);
+  } else if (SUB_IDENTITY_REMOVED === method) {
+    await deleteSubIdentity(event);
+    await setIdentityEventForTimeline(SUB_IDENTITY_REMOVED, event, indexer);
+  } else if (SUB_IDENTITY_REVOKED === method) {
+    await deleteSubIdentity(event);
+    await setIdentityEventForTimeline(SUB_IDENTITY_REVOKED, event, indexer);
   }
 }
 
