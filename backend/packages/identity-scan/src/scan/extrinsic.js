b2067bc89384cc9f8762564ed58d1469fd24cc8a
Yongfeng LI <wliyongfeng@gmail.com>
2023-06-22 00:53:27 +0800

    Remove useless calls handling

--- backend/packages/identity-scan/src/scan/extrinsic.js
+++ backend/packages/identity-scan/src/scan/extrinsic.js
@@ -4,16 +4,9 @@ const { handleSetFeeCall } = require("./calls/setFee");
 const { handleRenameSub } = require("./calls/renameSub");
 const { handleSetSubs } = require("./calls/setSubs");
 const {
-  handleSubIdentityExtrinsics,
-} = require("./calls/subIdentityExtrinsics");
-const {
   handleCallsInExtrinsic,
   utils: { isExtrinsicSuccess, extractExtrinsicEvents },
 } = require("@osn/scan-common");
-const {
-  SECTION: { IDENTITY },
-  EXTRINSIC_METHOD: { SET_SUBS, RENAME_SUB },
-} = require("./constants");
 
 async function handleCalls(call, author, extrinsicIndexer, wrappedEvents) {
   await handleSetSubs(call, author, extrinsicIndexer, wrappedEvents);
@@ -42,37 +35,6 @@ async function handleExtrinsics(extrinsics = [], allEvents = [], indexer) {
       handleCalls,
     );
   }
-}
-
-async function handleNestedExtrinsics(call, author, extrinsicIndexer) {
-  const [section, method] = [call.section, call.method];
-
-  if (isNotIdentityExtrinsic(section, method)) {
-    return;
-  }
-
-  const extrinsicData = call.toJSON().args;
-
-  await handleSubIdentityExtrinsics(
-    author,
-    extrinsicData,
-    extrinsicIndexer,
-    method,
-  );
-}
-
-async function handleIdentityCalls(
-  call,
-  author,
-  extrinsicIndexer,
-  wrappedEvents,
-) {
-  await handleNestedExtrinsics(...arguments);
-}
-
-// boolean function to check if the extrinsic is a setSubs or renameSub extrinsic and identity section
-function isNotIdentityExtrinsic(section, method) {
-  return section !== IDENTITY || (method !== SET_SUBS && method !== RENAME_SUB);
 }
 
 module.exports = {
