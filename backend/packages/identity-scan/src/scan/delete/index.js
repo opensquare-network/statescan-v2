f362e8c9f887f008d269e79b9713b9ec52ea96a8
Yongfeng LI <wliyongfeng@gmail.com>
2023-06-23 21:49:38 +0800

    Delete todo

--- backend/packages/identity-scan/src/scan/delete/index.js
+++ backend/packages/identity-scan/src/scan/delete/index.js
@@ -8,7 +8,6 @@ const {
 } = require("@statescan/mongo");
 
 async function deleteFrom(height) {
-  // todo: delete business generated from block whose height is >= height
   if (!height) {
     throw new Error("No height given when deleting unFinalized transfers");
   }
