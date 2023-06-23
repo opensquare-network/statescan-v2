af395f138aabf30190ddd6acda734b40850ba4c9
Yongfeng LI <wliyongfeng@gmail.com>
2023-06-23 22:12:38 +0800

    Fix handle assets events arguments

--- backend/packages/asset-scan/src/scan/events/assets/index.js
+++ backend/packages/asset-scan/src/scan/events/assets/index.js
@@ -17,7 +17,7 @@ const { handleMetadataSet } = require("./metadataSet");
 const { handleCreated } = require("./created");
 const { AssetsEvents } = require("./consts");
 
-async function handleAssetsEvent(event) {
+async function handleAssetsEvent(event, indexer, extrinsic) {
   const { section, method } = event;
 
   if (section !== "assets") {
