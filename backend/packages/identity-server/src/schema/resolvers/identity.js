36d0b2ee4c829ce21cd24bc5df3b73f2b126c66b
Yongfeng LI <wliyongfeng@gmail.com>
2023-07-04 11:40:37 +0800

    Polish identity resolver, #699

--- backend/packages/identity-server/src/schema/resolvers/identity.js
+++ backend/packages/identity-server/src/schema/resolvers/identity.js
@@ -1,6 +1,18 @@
+const {
+  identity: { getIdentityCol },
+} = require("@statescan/mongo");
+const {
+  utils: { isValidAddress },
+} = require("@statescan/common");
+
 async function identity(_, _args) {
-  console.log(_args);
-  return null;
+  const { account } = _args;
+  if (!isValidAddress(account)) {
+    return null;
+  }
+
+  const col = await getIdentityCol();
+  return await col.findOne({ account }, { projection: { _id: 0 } });
 }
 
 module.exports = {
