fd4a265064a2d4b69319c18ce75ea0d106cc7ffe
Yongfeng LI <wliyongfeng@gmail.com>
2023-07-13 11:07:22 +0800

    Save fee info in identity scan (#702)
    * Extract max fee when identy judgement requested
    
    * Calc fee when judgement given

--- backend/packages/identity-scan/src/scan/events/identity/events/judgementRequested.js
+++ backend/packages/identity-scan/src/scan/events/identity/events/judgementRequested.js
@@ -4,21 +4,50 @@ const {
   insertRequestTimeline,
 } = require("../../../mongo");
 const { handleJudgementCommon } = require("./common");
+const {
+  call: { findTargetCall },
+} = require("@osn/scan-common");
+const {
+  utils: { toDecimal128 },
+} = require("@statescan/common");
 
-async function handleJudgementRequested(event, indexer) {
+async function handleJudgementRequested(event, indexer, extrinsic) {
   const {
     account,
     registrar: { index: registrarIndex, account: registrar },
   } = await handleJudgementCommon(event, indexer);
 
+  let maxFee;
+  if (extrinsic) {
+    const call = findTargetCall(extrinsic.method, (call) => {
+      const { section, method, args: callArgs } = call;
+      return (
+        "identity" === section &&
+        "requestJudgement" === method &&
+        callArgs[0].toNumber() === registrarIndex
+      );
+    });
+
+    if (call) {
+      maxFee = call.args[1].toJSON();
+    }
+  }
+
+  let args = { registrarIndex };
+  if (maxFee) {
+    args = { ...args, maxFee };
+  }
   await insertJudgementRequest({
     account,
     registrarIndex,
     registrar,
+    requestHeight: indexer.blockHeight,
     indexer,
+    maxFee: toDecimal128(maxFee),
     status: {
       name: REQUEST_STATUS.PENDING,
       indexer,
+      args,
     },
     isFinal: false,
   });
@@ -29,9 +58,7 @@ async function handleJudgementRequested(event, indexer) {
     requestHeight: indexer.blockHeight,
     indexer,
     name: event.method,
-    args: {
-      registrarIndex,
-    },
+    args,
   });
 }
 
