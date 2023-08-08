9e42696634a8092045bef871d2f3845657f774c6
2nthony <ant.hu@outlook.com>
2023-08-08 16:38:45 +0800

    fetch data

--- site/src/pages/registrars.jsx
+++ site/src/pages/registrars.jsx
@@ -1,36 +1,66 @@
+import { gql, useQuery } from "@apollo/client";
+import { toPrecision } from "@osn/common";
 import { parseInt } from "lodash";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import AddressOrIdentity from "../components/address";
 import BreadCrumb from "../components/breadCrumb";
+import ValueDisplay from "../components/displayValue";
 import Layout from "../components/layout";
 import Pagination from "../components/pagination";
+import { Flex } from "../components/styled/flex";
 import { StyledPanelTableWrapper } from "../components/styled/panel";
 import Table from "../components/table";
-import {
-  registrarsFetchList,
-  registrarsListLoadingSelector,
-  registrarsListSelector,
-} from "../store/reducers/registrarsSlice";
 import { chainSettingSelector } from "../store/reducers/settingSlice";
+import { Overpass_Mono_14_500 } from "../styles/text";
 import { LIST_DEFAULT_PAGE_SIZE, registrarsHead } from "../utils/constants";
 import { getPageFromQuery } from "../utils/viewFuncs";
 
+const Index = styled.div`
+  ${Overpass_Mono_14_500};
+  color: ${(p) => p.theme.fontSecondary};
+`;
+
+const GET_REGISTRARS = gql`
+  query GetRegistrars {
+    registrars {
+      fee
+      statistics {
+        given
+        request
+        totalFee
+      }
+      index
+      account
+    }
+  }
+`;
+
 export default function RegistrarsPage() {
   const location = useLocation();
-  const dispatch = useDispatch();
   const chainSetting = useSelector(chainSettingSelector);
   const page = getPageFromQuery(location);
   const pageSize = LIST_DEFAULT_PAGE_SIZE;
 
-  const list = useSelector(registrarsListSelector);
-  const loading = useSelector(registrarsListLoadingSelector);
+  const { data, loading } = useQuery(GET_REGISTRARS);
 
-  useEffect(() => {
-    dispatch(registrarsFetchList(page - 1, pageSize));
-  }, [dispatch, page, pageSize]);
-
-  const data = [];
+  const tableData = data?.registrars.map((item) => {
+    return [
+      <Flex gap={24}>
+        <Index>#{item.index + 1}</Index>
+        <AddressOrIdentity address={item.account} />
+      </Flex>,
+      "",
+      "",
+      "",
+      <ValueDisplay
+        value={toPrecision(item.fee, chainSetting.decimals)}
+        symbol={chainSetting.symbol}
+      />,
+      "",
+    ];
+  });
 
   return (
     <Layout>
@@ -41,11 +71,11 @@ export default function RegistrarsPage() {
           <Pagination
             page={parseInt(page)}
             pageSize={pageSize}
-            total={list?.total}
+            total={data?.registrars?.length}
           />
         }
       >
-        <Table heads={registrarsHead} data={data} loading={loading} />
+        <Table heads={registrarsHead} data={tableData} loading={loading} />
       </StyledPanelTableWrapper>
     </Layout>
   );
