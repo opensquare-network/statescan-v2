5bc38165c30f0f72e8fc89b68fa7992f50eaeab6
2nthony <ant.hu@outlook.com>
2023-08-08 15:57:37 +0800

    fix: apollo client

--- site/src/context/provider.js
+++ site/src/context/provider.js
@@ -3,10 +3,20 @@ import { ThemeProvider } from "styled-components";
 import { modeSelector } from "../store/reducers/settingSlice";
 import light from "../styles/theme/light";
 import dark from "../styles/theme/dark";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_PUBLIC_IDENTITY_API_END_POINT + "graphql",
+  cache: new InMemoryCache(),
+});
 
 export default function GlobalProvider({ children }) {
   const mode = useSelector(modeSelector);
   const theme = mode === "light" ? light : dark;
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <ApolloProvider client={client}>{children}</ApolloProvider>
+    </ThemeProvider>
+  );
 }
