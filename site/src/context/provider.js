d865e958b5860bcfc2d0e222f764e50f109fba78
2nthony <ant.hu@outlook.com>
2023-08-09 11:22:38 +0800

    feat: global theme variables

--- site/src/context/provider.js
+++ site/src/context/provider.js
@@ -1,12 +1,28 @@
 import { useSelector } from "react-redux";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import { modeSelector } from "../store/reducers/settingSlice";
 import light from "../styles/theme/light";
 import dark from "../styles/theme/dark";
+
+function buildThemeVariables(theme = {}) {
+  return Object.keys(theme)
+    .map((key) => `--${key}: ${theme[key]};`)
+    .join("");
+}
+
+const GlobalThemeVariables = createGlobalStyle(({ theme }) => {
+  const themeVariables = buildThemeVariables(theme);
+  return `:root {${themeVariables}`;
+});
 
 export default function GlobalProvider({ children }) {
   const mode = useSelector(modeSelector);
   const theme = mode === "light" ? light : dark;
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalThemeVariables />
+      {children}
+    </ThemeProvider>
+  );
 }
