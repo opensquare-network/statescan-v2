b4efc1e7662f7aca606265d05e2643387b2ebcd9
2nthony <ant.hu@outlook.com>
2023-08-09 18:48:49 +0800

    stash

--- site/src/pages/identities.jsx
+++ site/src/pages/identities.jsx
@@ -1,16 +1,31 @@
 import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
 import BreadCrumb from "../components/breadCrumb";
 import Layout from "../components/layout";
 import { StyledPanelTableWrapper } from "../components/styled/panel";
 import Table from "../components/table";
 import { identitiesHead } from "../utils/constants";
 
-const GET_IDENTITIES = gql``;
+const GET_IDENTITIES = gql`
+  query GetIdentities {
+    identity(account: "Fom9M5W6Kck1hNAiE2mDcZ67auUCiNTzLBUdQy4QnxHSxdn") {
+      account
+      isSub
+      subs
+    }
+  }
+`;
 
 export default function IdentitiesPage() {
-  const { data, loading } = useQuery(GET_IDENTITIES);
+  const [data, setData] = useState(null);
 
-  const tableData = data?.identities.map((item) => {});
+  const { loading } = useQuery(GET_IDENTITIES, {
+    onCompleted(data) {
+      setData(data);
+    },
+  });
+
+  const tableData = data?.identity;
 
   return (
     <Layout>
