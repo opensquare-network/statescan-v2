b2eb61e59e04beabf8039103dc660ea9f2a16ba7
2nthony <ant.hu@outlook.com>
2023-08-10 17:03:34 +0800

    update

--- site/src/pages/identities.jsx
+++ site/src/pages/identities.jsx
@@ -1,34 +1,59 @@
 import { gql, useQuery } from "@apollo/client";
 import { useState } from "react";
 import BreadCrumb from "../components/breadCrumb";
+import Filter from "../components/filter";
 import Layout from "../components/layout";
+import Pagination from "../components/pagination";
 import { StyledPanelTableWrapper } from "../components/styled/panel";
 import Table from "../components/table";
-import { identitiesHead } from "../utils/constants";
+import { useQueryParams } from "../hooks/useQueryParams";
+import { identitiesHead, LIST_DEFAULT_PAGE_SIZE } from "../utils/constants";
+import { useIdentitiesFilter } from "../utils/hooks/identitiesFilter";
 
-// TODO: identities query
-/* const GET_IDENTITIES = gql`
-  query GetIdentities {
+// FIXME: identities query
+const GET_IDENTITIES = gql`
+  query GetIdentity($account: String!) {
+    identity(account: $account) {
+      account
+    }
   }
-`; */
+`;
 
 export default function IdentitiesPage() {
   const [data, setData] = useState(null);
+  const filter = useIdentitiesFilter();
+  const { page = 1, showSubIdentity = false, account = "" } = useQueryParams();
+  const pageSize = LIST_DEFAULT_PAGE_SIZE;
 
-  const loading = false;
-  /* const { loading } = useQuery(GET_IDENTITIES, {
+  const { loading } = useQuery(GET_IDENTITIES, {
+    variables: {
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
+      showSubIdentity,
+      account,
+    },
     onCompleted(data) {
       setData(data);
     },
-  }); */
+  });
 
-  const tableData = data?.identity;
+  const tableData = data?.identities ?? [];
 
   return (
     <Layout>
       <BreadCrumb data={[{ name: "Identities" }]} />
 
-      <StyledPanelTableWrapper>
+      <Filter data={filter} showFilterButton={false} filterOnDataChange />
+
+      <StyledPanelTableWrapper
+        footer={
+          <Pagination
+            page={page}
+            pageSize={pageSize}
+            total={data?.identities?.total}
+          />
+        }
+      >
         <Table heads={identitiesHead} data={tableData} loading={loading} />
       </StyledPanelTableWrapper>
     </Layout>
---
+++ site/src/utils/hooks/identitiesFilter.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { useQueryParams } from "../../hooks/useQueryParams";
+import SearchIcon from "../../components/icons/searchIcon";
+
+export function useIdentitiesFilter() {
+  const [filter, setFilter] = useState([]);
+  const { account = "", showSubIdentity = false } = useQueryParams();
+
+  useEffect(() => {
+    const searchFilter = {
+      value: account,
+      type: "input",
+      name: "Search",
+      query: "account",
+      inputProps: {
+        placeholder: "Address/Identity...",
+        prefix: <SearchIcon style={{ width: 16, height: 16 }} />,
+      },
+    };
+
+    const showSubIdentityFilter = {
+      value: showSubIdentity === "true",
+      type: "checkbox",
+      name: "Show Sub Identity",
+      query: "showSubIdentity",
+    };
+
+    setFilter([searchFilter, { type: "divider" }, showSubIdentityFilter]);
+  }, [account, showSubIdentity]);
+
+  return filter;
+}
