ac066b5aa91741b71a43dc3712408246c6d32d59
2nthony <ant.hu@outlook.com>
2023-08-10 14:30:55 +0800

    disable logic

--- site/src/pages/identities.jsx
+++ site/src/pages/identities.jsx
@@ -6,24 +6,21 @@ import { StyledPanelTableWrapper } from "../components/styled/panel";
 import Table from "../components/table";
 import { identitiesHead } from "../utils/constants";
 
-const GET_IDENTITIES = gql`
+// TODO: identities query
+/* const GET_IDENTITIES = gql`
   query GetIdentities {
-    identity(account: "Fom9M5W6Kck1hNAiE2mDcZ67auUCiNTzLBUdQy4QnxHSxdn") {
-      account
-      isSub
-      subs
-    }
   }
-`;
+`; */
 
 export default function IdentitiesPage() {
   const [data, setData] = useState(null);
 
-  const { loading } = useQuery(GET_IDENTITIES, {
+  const loading = false;
+  /* const { loading } = useQuery(GET_IDENTITIES, {
     onCompleted(data) {
       setData(data);
     },
-  });
+  }); */
 
   const tableData = data?.identity;
 
