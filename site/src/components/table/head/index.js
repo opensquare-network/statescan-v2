fa7afe5225665fda901369225ec767d833ae64f0
2nthony <ant.hu@outlook.com>
2023-08-11 18:25:53 +0800

    feat: table sortable (#720)
    * sort icons
    
    * fix svg default color
    
    * feat: table support sort
    
    * fix: filter support sort
    
    * fix: sortable head init value

--- site/src/components/table/head/index.js
+++ site/src/components/table/head/index.js
@@ -10,6 +10,8 @@ import { useEffect } from "react";
 import { smcss } from "../../../styles/responsive";
 import { w, w_full } from "../../../styles/tailwindcss";
 import last from "lodash.last";
+import SortableHead from "./sortableHead";
+import { useState } from "react";
 
 const Tr = styled.tr`
   border-bottom: 1px solid ${(p) => p.theme.strokeBase};
@@ -37,6 +39,7 @@ export default function TableHead({ heads }) {
   const dispatch = useDispatch();
   const timeType = useSelector(timeTypeSelector);
   const isDataTableTypeLast = last(heads)?.type === "data";
+  const [activeSortQueryValue, setActiveSortQueryValue] = useState("");
 
   useEffect(() => {
     const timeType = localStorage.getItem("timeType");
@@ -52,7 +55,9 @@ export default function TableHead({ heads }) {
   return (
     <thead>
       <Tr>
-        {heads.map(({ name, align = "left", type, width }, index) => {
+        {heads.map((head, index) => {
+          let { name, align = "left", type, width } = head ?? {};
+
           const isSecondToLast = heads.length - 2 === index;
           const shouldFlexWidthSecondToLast =
             isDataTableTypeLast && isSecondToLast;
@@ -63,6 +68,19 @@ export default function TableHead({ heads }) {
               <TimeHead timeType={timeType} setTimeType={doSetTimeType} />
             );
           }
+
+          if (type === "sortable") {
+            content = (
+              <SortableHead
+                activeSortQueryValue={activeSortQueryValue}
+                setActiveSortQueryValue={setActiveSortQueryValue}
+                head={head}
+              >
+                {name}
+              </SortableHead>
+            );
+          }
+
           if (type === "data") {
             width = width ?? 76;
             content = "";
---
+++ site/src/components/table/head/sortableHead.jsx
@@ -0,0 +1,95 @@
+import noop from "lodash.noop";
+import styled, { css } from "styled-components";
+import { Flex } from "../../styled/flex";
+import { ReactComponent as SortAscend } from "../../icons/sort-ascend.svg";
+import { ReactComponent as SortDescend } from "../../icons/sort-descend.svg";
+import { useState } from "react";
+import { useQueryParams } from "../../../hooks/useQueryParams";
+import omit from "lodash.omit";
+import { useNavigate } from "react-router-dom";
+import { serialize } from "../../../utils/viewFuncs";
+import { useEffectOnce, useUpdateEffect } from "usehooks-ts";
+
+const Wrapper = styled(Flex)`
+  column-gap: 4px;
+  cursor: pointer;
+
+  ${(p) =>
+    p.reverse &&
+    css`
+      flex-direction: row-reverse;
+    `}
+
+  ${(p) =>
+    p.active &&
+    css`
+      color: var(--fontSecondary);
+
+      svg {
+        path {
+          fill: var(--fontSecondary);
+        }
+      }
+    `}
+`;
+
+export default function SortableHead({
+  activeSortQueryValue,
+  setActiveSortQueryValue = noop,
+  children,
+  head,
+}) {
+  const { align, sortQueryValue, sortDefaultDirection } = head ?? {};
+
+  const navigate = useNavigate();
+  const params = useQueryParams();
+
+  const [active, setActive] = useState();
+  useEffectOnce(() => {
+    const urlKey = params.ascendingBy || params.descendingBy;
+    if (urlKey) {
+      setActive(urlKey === sortQueryValue);
+    } else if (sortDefaultDirection) {
+      setActive(!!sortDefaultDirection);
+    }
+  });
+  useUpdateEffect(() => {
+    setActive(activeSortQueryValue === sortQueryValue);
+  }, [activeSortQueryValue, sortQueryValue]);
+
+  const [descending, setDescending] = useState(true);
+  useEffectOnce(() => {
+    if (params.ascendingBy === sortQueryValue) {
+      setDescending(false);
+    } else if (sortDefaultDirection) {
+      setDescending(sortDefaultDirection === "descending");
+    } else {
+      setDescending(true);
+    }
+  });
+
+  function handleSort() {
+    setActiveSortQueryValue(sortQueryValue);
+    const newParams = omit(params, ["ascendingBy", "descendingBy"]);
+
+    if (!active) {
+      setDescending(true);
+      newParams["descendingBy"] = sortQueryValue;
+    } else {
+      const val = !descending;
+      setDescending(val);
+      newParams[val ? "descendingBy" : "ascendingBy"] = sortQueryValue;
+    }
+
+    const search = serialize(newParams);
+    navigate({ search });
+  }
+
+  return (
+    <Wrapper active={active} reverse={align === "right"} onClick={handleSort}>
+      {children}
+
+      {descending ? <SortDescend /> : <SortAscend />}
+    </Wrapper>
+  );
+}
