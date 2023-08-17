e2a4e1015b3fb1810c409782ed14a2bdc99dd9d5
2nthony <ant.hu@outlook.com>
2023-08-17 12:01:04 +0800

    fix: sortable head, query change to `sort`

--- site/src/components/table/head/sortableHead.jsx
+++ site/src/components/table/head/sortableHead.jsx
@@ -3,12 +3,13 @@ import styled, { css } from "styled-components";
 import { Flex } from "../../styled/flex";
 import { ReactComponent as SortAscend } from "../../icons/sort-ascend.svg";
 import { ReactComponent as SortDescend } from "../../icons/sort-descend.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQueryParams } from "../../../hooks/useQueryParams";
 import omit from "lodash.omit";
 import { useNavigate } from "react-router-dom";
 import { serialize } from "../../../utils/viewFuncs";
 import { useEffectOnce, useUpdateEffect } from "usehooks-ts";
+import { TABLE_SORT_QUERY_KEY } from "../../../utils/constants";
 
 const Wrapper = styled(Flex)`
   column-gap: 4px;
@@ -39,48 +40,70 @@ export default function SortableHead({
   children,
   head,
 }) {
-  const { align, sortQueryValue, sortDefaultDirection } = head ?? {};
+  const {
+    align,
+    sortDefaultQueryValue,
+    sortAscendingQueryValue,
+    sortDescendingQueryValue,
+  } = head ?? {};
 
   const navigate = useNavigate();
   const params = useQueryParams();
 
   const [active, setActive] = useState();
-  useEffectOnce(() => {
-    const urlKey = params.ascendingBy || params.descendingBy;
-    if (urlKey) {
-      setActive(urlKey === sortQueryValue);
-    } else if (sortDefaultDirection) {
-      setActive(!!sortDefaultDirection);
+  useEffect(() => {
+    if (params[TABLE_SORT_QUERY_KEY]) {
+      if (
+        params[TABLE_SORT_QUERY_KEY] === sortAscendingQueryValue ||
+        params[TABLE_SORT_QUERY_KEY] === sortDescendingQueryValue
+      ) {
+        setActive(true);
+      }
+    } else if (sortDefaultQueryValue) {
+      setActive(true);
     }
-  });
+  }, [
+    params,
+    sortAscendingQueryValue,
+    sortDefaultQueryValue,
+    sortDescendingQueryValue,
+  ]);
   useUpdateEffect(() => {
-    setActive(activeSortQueryValue === sortQueryValue);
-  }, [activeSortQueryValue, sortQueryValue]);
+    setActive(
+      activeSortQueryValue === sortAscendingQueryValue ||
+        activeSortQueryValue === sortDescendingQueryValue,
+    );
+  }, [activeSortQueryValue, sortAscendingQueryValue, sortDescendingQueryValue]);
 
   const [descending, setDescending] = useState(true);
   useEffectOnce(() => {
-    if (params.ascendingBy === sortQueryValue) {
+    if (
+      params[TABLE_SORT_QUERY_KEY] === sortAscendingQueryValue ||
+      sortDefaultQueryValue === sortAscendingQueryValue
+    ) {
       setDescending(false);
-    } else if (params.descendingBy === sortQueryValue) {
+    } else if (params[TABLE_SORT_QUERY_KEY] === sortDescendingQueryValue) {
       setDescending(true);
-    } else if (sortDefaultDirection) {
-      setDescending(sortDefaultDirection === "descending");
     } else {
       setDescending(true);
     }
   });
 
   function handleSort() {
-    setActiveSortQueryValue(sortQueryValue);
-    const newParams = omit(params, ["ascendingBy", "descendingBy"]);
+    const newParams = omit(params, [TABLE_SORT_QUERY_KEY]);
 
     if (!active) {
       setDescending(true);
-      newParams["descendingBy"] = sortQueryValue;
+      newParams[TABLE_SORT_QUERY_KEY] = sortDescendingQueryValue;
+      setActiveSortQueryValue(sortDescendingQueryValue);
     } else {
       const val = !descending;
+      const direction = val
+        ? sortDescendingQueryValue
+        : sortAscendingQueryValue;
       setDescending(val);
-      newParams[val ? "descendingBy" : "ascendingBy"] = sortQueryValue;
+      newParams[TABLE_SORT_QUERY_KEY] = direction;
+      setActiveSortQueryValue(direction);
     }
 
     const search = serialize(newParams);
