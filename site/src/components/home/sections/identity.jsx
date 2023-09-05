d09dcaeb060aa789fbd356ac4697900f26be8b3d
2nthony <ant.hu@outlook.com>
2023-09-05 15:36:00 +0800

    stash

--- site/src/components/home/sections/identity.jsx
+++ site/src/components/home/sections/identity.jsx
@@ -1,5 +1,35 @@
-import { OverviewPanel } from "../overview/styled";
+import DataIdentityIcon from "../../icons/dataIdentity";
+import DataRegistrarsIcon from "../../icons/dataRegistrars";
+import DataRequestsIcon from "../../icons/dataRequests";
+import DataSubIdentityIcon from "../../icons/dataSubIdentity";
+import OverviewItem from "../overview/item";
+import { OverviewItemsWrapper, OverviewPanel } from "../overview/styled";
 
 export default function IdentitySection() {
-  return <OverviewPanel>Identity Section</OverviewPanel>;
+  return (
+    <OverviewPanel>
+      <OverviewItemsWrapper>
+        <OverviewItem
+          icon={<DataIdentityIcon />}
+          label="Total identities"
+          value={<div>1</div>}
+        />
+        <OverviewItem
+          icon={<DataSubIdentityIcon />}
+          label="Total sub identities"
+          value={<div>1</div>}
+        />
+        <OverviewItem
+          icon={<DataRegistrarsIcon />}
+          label="Total registrars"
+          value={<div>1</div>}
+        />
+        <OverviewItem
+          icon={<DataRequestsIcon />}
+          label="Requests"
+          value={<div>1</div>}
+        />
+      </OverviewItemsWrapper>
+    </OverviewPanel>
+  );
 }
