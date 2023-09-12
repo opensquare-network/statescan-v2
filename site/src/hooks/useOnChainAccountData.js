f4a96169a5d046814d5df4423f1b65d81b65fe82
hyifeng <hyifeng@gmail.com>
2023-09-12 14:23:27 +0800

    fix: onchain block 404 and invalid account 404 displaying (#794)
    * fix: onchain block 404 and account 404 displaying
    
    * Update

--- site/src/hooks/useOnChainAccountData.js
+++ site/src/hooks/useOnChainAccountData.js
@@ -1,50 +1,42 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useChainApi } from "../utils/hooks/chain/useChainApi";
 
 export default function useOnChainAccountData(address) {
   const api = useChainApi();
-  const [account, setAccount] = useState();
-  const [balanceAll, setBalanceAll] = useState();
-  const [stakingInfo, setStakingInfo] = useState();
+  const [accountData, setAccountData] = useState();
 
-  useEffect(() => {
+  const fetchAccountData = useCallback(async () => {
     if (!api || !address) {
       return;
     }
 
-    api.query.system.account(address).then((data) => {
-      const accountInfo = api.registry.createType(
-        "AccountInfo",
-        data.toHex(),
-        true,
-      );
-      setAccount(accountInfo);
+    const [account, balanceAll, stakingInfo] = await Promise.all([
+      api.query.system.account(address),
+      api.derive.balances?.all(address).catch(() => null),
+      api.derive.staking?.account(address).catch(() => null),
+    ]);
+
+    if (!account) {
+      setAccountData(null);
+      return;
+    }
+
+    const accountInfo = api.registry.createType(
+      "AccountInfo",
+      account.toHex(),
+      true,
+    );
+
+    setAccountData({
+      account: accountInfo,
+      balanceAll,
+      stakingInfo,
     });
-
-    if (api.derive.balances?.all) {
-      api.derive.balances?.all(address).then((data) => {
-        setBalanceAll(data);
-      });
-    } else {
-      setBalanceAll(null);
-    }
-
-    if (api.derive.staking?.account) {
-      api.derive.staking?.account(address).then((data) => {
-        setStakingInfo(data);
-      });
-    } else {
-      setStakingInfo(null);
-    }
   }, [api, address]);
 
-  if (
-    account === undefined ||
-    balanceAll === undefined ||
-    stakingInfo === undefined
-  ) {
-    return null;
-  }
+  useEffect(() => {
+    fetchAccountData().catch(() => setAccountData(null));
+  }, [fetchAccountData]);
 
-  return { account, balanceAll, stakingInfo };
+  return accountData;
 }
