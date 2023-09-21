d7f2c340589bd831271887f6cb68282df64beb87
Yongfeng LI <wliyongfeng@gmail.com>
2023-09-21 11:59:32 +0800

    Add usdc definition file, #807

---
+++ site/src/utils/consts/assetInfoData/statemint/usdc-1337.js
@@ -0,0 +1,30 @@
+export const USDC = {
+  id: 1337,
+  height: 2616910,
+  data: {
+    icon: "/imgs/icons/asset/usdc.svg",
+    about:
+      "USD Coin (known by its ticker USDC) is a stablecoin that is pegged to the U.S. dollar on a 1:1 basis. Every unit of this cryptocurrency in circulation is backed up by $1 that is held in reserve, in a mix of cash and short-term U.S. Treasury bonds. The Centre consortium, which is behind this asset, says USDC is issued by regulated financial institutions.",
+    links: [
+      {
+        name: "Website",
+        url: "https://www.circle.com/blog/now-available-usdc-on-polkadot-asset-hub",
+      },
+      {
+        name: "Twitter: @circle",
+        url: "https://twitter.com/circle",
+        icon: "/imgs/icons/link-twitter.svg",
+      },
+      {
+        name: "Medium",
+        url: "https://medium.com/centre-blog",
+        icon: "/imgs/icons/link-medium.svg",
+      },
+      {
+        name: "Github",
+        url: "https://github.com/circlefin",
+        icon: "/imgs/icons/link-github.svg",
+      },
+    ],
+  },
+};
