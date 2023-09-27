bc713533e5b08a17839ec4dc9b915bec535603e7
2nthony <ant.hu@outlook.com>
2023-09-27 14:20:54 +0800

    fix: Improve NFT small logo, #812

--- site/src/components/nft/thumbnail/index.js
+++ site/src/components/nft/thumbnail/index.js
@@ -3,6 +3,8 @@ import styled, { css } from "styled-components";
 import { ReactComponent as NftUnrecognizedThumbnailSvg } from "./unrecognized-thumbnail.svg";
 
 const Wrapper = styled.div`
+  width: ${(p) => p.size}px;
+  height: ${(p) => p.size}px;
   ${(props) =>
     props.onClick !== noop &&
     css`
@@ -14,8 +16,8 @@ const ThumbnailWrapper = styled(Wrapper)`
   display: flex;
   justify-content: space-around;
   align-items: center;
-  min-width: ${(p) => p.size}px;
-  min-height: ${(p) => p.size}px;
+  width: ${(p) => p.size}px;
+  height: ${(p) => p.size}px;
   border-radius: 5px;
   overflow: hidden;
   background-color: ${(props) => props.background ?? props.theme.fillSub};
@@ -34,10 +36,10 @@ export default function Thumbnail({
 }) {
   return image ? (
     <ThumbnailWrapper size={size} onClick={onClick}>
-      <img width={size} height={size} src={image} alt="" />
+      <img width="100%" src={image} alt="" />
     </ThumbnailWrapper>
   ) : (
-    <Wrapper onClick={onClick}>
+    <Wrapper size={size} onClick={onClick}>
       <NftUnrecognizedThumbnailSvg
         width={size}
         height={size}
