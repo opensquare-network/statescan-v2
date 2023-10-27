987c8c8fd6ada11de5cde08a298de666dc169b8d
2nthony <ant.hu@outlook.com>
2023-10-27 18:18:57 +0800

    stash timeline

--- site/src/components/multisig/timeline.jsx
+++ site/src/components/multisig/timeline.jsx
@@ -1,11 +1,17 @@
 import { useMultisigTimelineData } from "../../hooks/multisig/useMultisigTimelineData";
+import { MULTISIG_NAME } from "../../utils/constants";
+import AddressOrIdentity from "../address";
 import Timeline from "../timeline";
+import TimelineItemFields from "../timeline/itemFields";
 
 const iconsSrcs = {
-  NewMultisig: "/imgs/icons/timeline-new-multisig.svg",
-  MultisigApproval: "/imgs/icons/timeline-multisig-approval.svg",
-  MultisigExecuted: "/imgs/icons/timeline-multisig-executed.svg",
-  MultisigCancelled: "/imgs/icons/timeline-multisig-cancelled.svg",
+  [MULTISIG_NAME.NewMultisig]: "/imgs/icons/timeline-new-multisig.svg",
+  [MULTISIG_NAME.MultisigApproval]:
+    "/imgs/icons/timeline-multisig-approval.svg",
+  [MULTISIG_NAME.MultisigExecuted]:
+    "/imgs/icons/timeline-multisig-executed.svg",
+  [MULTISIG_NAME.MultisigCancelled]:
+    "/imgs/icons/timeline-multisig-cancelled.svg",
 };
 
 export default function MultisigTimeline() {
@@ -36,5 +42,24 @@ function Icon({ name }) {
 }
 
 function Fields({ item }) {
-  return <div>fields</div>;
+  const fields = [
+    item.name === MULTISIG_NAME.NewMultisig && [
+      "Multisig Member",
+      <AddressOrIdentity address={item.args.approving} />,
+    ],
+    item.name === MULTISIG_NAME.MultisigApproval && [
+      "Multisig Member",
+      <AddressOrIdentity address={item.args.approving} />,
+    ],
+    item.name === MULTISIG_NAME.MultisigExecuted && [
+      "Multisig Member",
+      <AddressOrIdentity address={item.args.approving} />,
+    ],
+    item.name === MULTISIG_NAME.MultisigCancelled && [
+      "Multisig Member",
+      <AddressOrIdentity address={item.args.cancelling} />,
+    ],
+  ].filter(Boolean);
+
+  return <TimelineItemFields fields={fields} />;
 }
