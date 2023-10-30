edfc75bfa7958ea8018df6c7db6a47b3eed53ec6
2nthony <ant.hu@outlook.com>
2023-10-30 18:46:49 +0800

    fix: account signatories table

--- site/src/components/accountMultisig/signatories.jsx
+++ site/src/components/accountMultisig/signatories.jsx
@@ -1,10 +1,15 @@
 import AddressOrIdentity from "../address";
+import { Flex } from "../styled/flex";
 import { StyledPanelTableWrapperNoBordered } from "../styled/panel";
 import Table from "../table";
 
 export default function AccountTabMultisigSignatories({ signatories = [] }) {
   const tableData = signatories.map((address) => {
-    return [<AddressOrIdentity address={address} />];
+    return [
+      <Flex>
+        <AddressOrIdentity address={address} />
+      </Flex>,
+    ];
   });
 
   return (
