f3b54c3762ddfc1938f8846b6df535e903a53dcc
chaojun <chaojun@opensquare.network>
2023-11-02 13:46:38 +0800

    Update remove simple mode hint

--- site/src/components/home/sections/blockAndTransfers/blocks.js
+++ site/src/components/home/sections/blockAndTransfers/blocks.js
@@ -7,15 +7,6 @@ import { Anchor, AnchorWrapper, Section, StyledPanel, Title } from "../styled";
 import React from "react";
 import LatestBlocks from "../latestBlocks";
 import { getIsSimpleMode } from "../../../../utils/env";
-import styled from "styled-components";
-import { Inter_14_500 } from "../../../../styles/text";
-
-const SimpleModeHint = styled.div`
-  display: flex;
-  flex-grow: 1;
-  ${Inter_14_500};
-  color: ${(props) => props.theme.fontSecondary};
-`;
 
 export default function Blocks() {
   const blocks = useSelector(latestBlocksSelector);
@@ -30,7 +21,6 @@ export default function Blocks() {
         <AnchorWrapper
           disabled={isSimpleMode || !blocks.length || blocksLoading}
         >
-          <SimpleModeHint>*Currently in simple mode</SimpleModeHint>
           <Anchor to={"/blocks"}>View All</Anchor>
         </AnchorWrapper>
       </StyledPanel>
