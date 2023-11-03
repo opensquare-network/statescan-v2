0bfcfb3841f1973f12f265d5ecceaa4c393117af
2nthony <ant.hu@outlook.com>
2023-11-03 18:14:11 +0800

    redux, current filter value

--- site/src/store/reducers/filterSlice.js
+++ site/src/store/reducers/filterSlice.js
@@ -5,17 +5,23 @@ const filterSlice = createSlice({
   name: "filter",
   initialState: {
     filters: null,
+    currentFilterValue: {},
   },
   reducers: {
     setFilters(state, { payload }) {
       state.filters = payload;
     },
+    setCurrentFilterValue(state, { payload }) {
+      state.currentFilterValue = payload;
+    },
   },
 });
 
 export const filtersSelector = (state) => state.filter.filters;
+export const currentFilterValueSelector = (state) =>
+  state.filter.currentFilterValue;
 
-export const { setFilters } = filterSlice.actions;
+export const { setFilters, setCurrentFilterValue } = filterSlice.actions;
 
 export const fetchSpecsFilter = () => async (dispatch) => {
   return api.fetch(`/specs/filter`).then(({ result }) => {
