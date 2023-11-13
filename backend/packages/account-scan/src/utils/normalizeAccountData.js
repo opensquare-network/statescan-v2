6d129566e66f95a4501d6e1a25d034cfeb7a3f89
chaojun <chaojun@opensquare.network>
2023-11-13 13:03:59 +0800

    fix: feeFrozen field is not a valid Decimal128 string - inexact rounding

--- backend/packages/account-scan/src/utils/normalizeAccountData.js
+++ backend/packages/account-scan/src/utils/normalizeAccountData.js
@@ -1,5 +1,7 @@
 const { toDecimal128 } = require("./toDecimal128");
-const { utils: { bigAdd } } = require("@osn/scan-common");
+const {
+  utils: { bigAdd },
+} = require("@osn/scan-common");
 
 function normalizeData(accountData) {
   const { free, reserved, miscFrozen, feeFrozen } = accountData;
@@ -10,10 +12,10 @@ function normalizeData(accountData) {
     free: toDecimal128(free),
     reserved: toDecimal128(reserved),
     miscFrozen: toDecimal128(miscFrozen),
-    feeFrozen: toDecimal128(feeFrozen),
+    feeFrozen,
   };
 }
 
 module.exports = {
   normalizeData,
-}
+};
