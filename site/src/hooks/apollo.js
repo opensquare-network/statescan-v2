8dea24f0252f3b707ed709a1b5ea53740867ec20
2nthony <ant.hu@outlook.com>
2023-11-20 16:37:34 +0800

    fix: identity hooks safe call

--- site/src/hooks/apollo.js
+++ site/src/hooks/apollo.js
@@ -1,9 +1,4 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  useQuery,
-  useLazyQuery,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, useLazyQuery } from "@apollo/client";
 import noop from "lodash.noop";
 import { useEffect } from "react";
 import useChainSettings from "../utils/hooks/chain/useChainSettings";
@@ -22,15 +17,35 @@ const multisigClient = new ApolloClient({
  * @type {typeof useQuery}
  */
 export function useIdentityQuery(query, options = {}, ...args) {
-  options.client = options.client || identityClient;
-  return useQuery(query, options, ...args);
+  const { modules } = useChainSettings();
+  const [fetcher, lazyQueryResult] = useIdentityLazyQuery(
+    query,
+    options,
+    ...args,
+  );
+
+  useEffect(() => {
+    if (modules.identity) {
+      fetcher();
+    }
+  }, [query, options, args, modules.identity, fetcher]);
+
+  return lazyQueryResult;
 }
 /**
  * @type {typeof useLazyQuery}
  */
 export function useIdentityLazyQuery(query, options = {}, ...args) {
   options.client = options.client || identityClient;
-  return useLazyQuery(query, options, ...args);
+
+  const { modules } = useChainSettings();
+  let [fetcher, lazyQueryResult] = useLazyQuery(query, options, ...args);
+
+  if (!modules.identity) {
+    fetcher = noop;
+  }
+
+  return [fetcher, lazyQueryResult];
 }
 
 /**
