c784b5ec9e36fbc6576f9206089e3592a06d7513
2nthony <ant.hu@outlook.com>
2023-11-20 16:34:40 +0800

    fix: multisig hooks safe call

--- site/src/hooks/apollo.js
+++ site/src/hooks/apollo.js
@@ -4,6 +4,9 @@ import {
   useQuery,
   useLazyQuery,
 } from "@apollo/client";
+import noop from "lodash.noop";
+import { useEffect } from "react";
+import useChainSettings from "../utils/hooks/chain/useChainSettings";
 
 const identityClient = new ApolloClient({
   uri: process.env.REACT_APP_PUBLIC_IDENTITY_API_END_POINT + "graphql",
@@ -35,13 +38,35 @@ export function useIdentityLazyQuery(query, options = {}, ...args) {
  */
 export function useMultisigQuery(query, options = {}, ...args) {
   options.client = options.client || multisigClient;
-  return useQuery(query, options, ...args);
+
+  const { modules } = useChainSettings();
+  const [fetcher, lazyQueryResult] = useMultisigLazyQuery(
+    query,
+    options,
+    ...args,
+  );
+
+  useEffect(() => {
+    if (modules.multisig) {
+      fetcher();
+    }
+  }, [query, options, args, modules.multisig, fetcher]);
+
+  return lazyQueryResult;
 }
 
 /**
- * @type {typeof useQuery}
+ * @type {typeof useLazyQuery}
  */
 export function useMultisigLazyQuery(query, options = {}, ...args) {
   options.client = options.client || multisigClient;
-  return useLazyQuery(query, options, ...args);
+
+  const { modules } = useChainSettings();
+  let [fetcher, lazyQueryResult] = useLazyQuery(query, options, ...args);
+
+  if (!modules.multisig) {
+    fetcher = noop;
+  }
+
+  return [fetcher, lazyQueryResult];
 }
