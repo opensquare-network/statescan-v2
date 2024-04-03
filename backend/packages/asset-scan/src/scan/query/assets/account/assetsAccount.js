e5479986d923511e75b9ea475c06ee1d3e10dca0
Yongfeng LI <wliyongfeng@gmail.com>
2024-04-03 14:10:51 +0800

    Improve the way to query assets account data (#870)
    * Improve the way to query assets account data
    
    * Guard assets account balance

--- backend/packages/asset-scan/src/scan/query/assets/account/assetsAccount.js
+++ backend/packages/asset-scan/src/scan/query/assets/account/assetsAccount.js
@@ -1,30 +1,27 @@
 const {
-  chain: { findBlockApi, getApi },
+  chain: { findBlockApi },
 } = require("@osn/scan-common");
-const { getAssetAccountStorageKey } = require("./key");
+
+async function queryAssetAccount(blockApi, assetId, address) {
+  const rawAccountInfo = await blockApi.query.assets.account(assetId, address);
+  return {
+    address,
+    info: rawAccountInfo.toJSON(),
+  };
+}
 
 async function queryAssetsAccounts(assetId, addresses = [], blockHash) {
   if (addresses.length <= 0) {
     return [];
   }
 
-  const api = await getApi();
   const blockApi = await findBlockApi(blockHash);
-  const registry = blockApi.registry;
-  const keys = addresses.map((address) =>
-    getAssetAccountStorageKey(assetId, address, registry),
-  );
-  const storageArray = await api.rpc.state.queryStorageAt(keys, blockHash);
+  const promises = [];
+  for (const address of addresses) {
+    promises.push(queryAssetAccount(blockApi, assetId, address));
+  }
 
-  return addresses.map((address, index) => {
-    const storage = storageArray[index];
-    let info = registry.createType("AssetBalance", storage.toHex(), true);
-
-    return {
-      address,
-      info: info.toJSON(),
-    };
-  });
+  return Promise.all(promises);
 }
 
 module.exports = {
