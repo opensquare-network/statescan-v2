73bbed452034bfef0a8ad6c24bc7462f6daf809b
Yongfeng LI <wliyongfeng@gmail.com>
2024-04-08 20:49:48 +0800

    Add resources for asset DED, #867

---
+++ site/src/utils/consts/assetInfoData/statemint/ded-30.js
@@ -0,0 +1,24 @@
+export const Ded = {
+  id: 30,
+  height: 5889939,
+  data: {
+    icon: "/imgs/icons/asset/ded.svg",
+    about: "$DED is a DOT Meme Coin: If you have DOT you get $DED Airdrop!",
+    links: [
+      {
+        name: "Website",
+        url: "https://www.dotisded.io/",
+      },
+      {
+        name: "Twitter: @dotisded",
+        url: "https://twitter.com/dotisded",
+        icon: "/imgs/icons/link-twitter.svg",
+      },
+      {
+        name: "Telegram: dedbird",
+        url: "https://t.me/dedbird",
+        icon: "/imgs/icons/link-telegram.svg",
+      },
+    ],
+  },
+};
--- site/src/utils/consts/assetInfoData/statemint/index.js
+++ site/src/utils/consts/assetInfoData/statemint/index.js
@@ -1,10 +1,12 @@
 import { assetUSDt } from "./usdt-1984";
 import { USDC } from "./usdc-1337";
+import { Ded } from "./ded-30";
 import { constructAssetId } from "../statemine";
 
 const statemintAssetInfo = {
   [constructAssetId(assetUSDt)]: assetUSDt.data,
   [constructAssetId(USDC)]: USDC.data,
+  [constructAssetId(Ded)]: Ded.data,
 };
 
 export default statemintAssetInfo;
