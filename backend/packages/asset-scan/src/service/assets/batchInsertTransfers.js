53575595ae5b03b74c82018eead50aa4fe971b34
Yongfeng LI <wliyongfeng@gmail.com>
2024-04-09 18:01:52 +0800

    Guard asset when inserting transfers, #874

--- backend/packages/asset-scan/src/service/assets/batchInsertTransfers.js
+++ backend/packages/asset-scan/src/service/assets/batchInsertTransfers.js
@@ -6,6 +6,7 @@ const {
 const {
   asset: { getTransferCollection },
 } = require("@statescan/mongo");
+const { logger } = require("@osn/scan-common");
 
 async function batchInsertAssetsTransfers(indexer) {
   const transfers = getAssetsTransfers(indexer.blockHash);
@@ -15,17 +16,19 @@ async function batchInsertAssetsTransfers(indexer) {
 
   const assetIds = [...new Set(transfers.map((item) => item.assetId))];
   const promises = assetIds.map((assetId) => getActiveAsset(assetId));
-  const assets = await Promise.all(promises);
+  const assets = (await Promise.all(promises)).filter(Boolean);
 
   const col = await getTransferCollection();
   const bulk = col.initializeUnorderedBulkOp();
   for (const transfer of transfers) {
     const asset = assets.find((asset) => asset.assetId === transfer.assetId);
     if (!asset) {
-      throw new Error(
+      logger.error(
         `Can not find asset: ${transfer.assetId} when insert transfer at ${indexer.blockHeight}`,
       );
+      continue;
     }
+
     bulk.insert({
       ...transfer,
       assetHeight: asset.assetHeight,
