e6f96414a38f93cd118db06c12046bf1443b4454
Yongfeng LI <wliyongfeng@gmail.com>
2024-04-29 16:11:04 +0800

    Support crust mainnet

---
+++ site/src/utils/consts/chains/crust.js
@@ -0,0 +1,27 @@
+import CrustIcon from "../../../components/icons/crustIcon";
+
+const nodes = [
+  {
+    name: "Crust",
+    url: "wss://rpc2-subscan.crust.network",
+  },
+];
+
+const crust = {
+  name: "Crust",
+  icon: <CrustIcon />,
+  identity: "crust",
+  value: "crust",
+  symbol: "CRU",
+  decimals: 12,
+  color: "#FA8C16",
+  colorSecondary: "rgba(250,140,22, 0.1)",
+  modules: {
+    identity: false,
+    multisig: false,
+  },
+  nodes,
+  useOnChainBlockData: true,
+};
+
+export default crust;
