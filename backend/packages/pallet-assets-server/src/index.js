9d1317cc32f3b932af2b9f74bb9576533319b340
Yongfeng LI <wliyongfeng@gmail.com>
2024-05-08 13:25:55 +0800

    Init asset db first, #886

--- backend/packages/pallet-assets-server/src/index.js
+++ backend/packages/pallet-assets-server/src/index.js
@@ -3,10 +3,15 @@ require("dotenv").config();
 const { schema } = require("./schema");
 const { createYoga } = require("graphql-yoga");
 const { createServer } = require("http");
+const {
+  palletAsset: { initPalletAssetScanDb },
+} = require("@statescan/mongo");
+const { assertWrappingType } = require("graphql/type");
 
 const port = parseInt(process.env.PORT) || 5100;
 
 function main() {
+  initPalletAssetScanDb().then(() => console.log("asset db initialized"));
   const yoga = createYoga({ schema });
   const server = createServer(yoga);
   server.listen(port, () => {
