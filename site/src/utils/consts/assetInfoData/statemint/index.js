0e04079dad937971dffbf7d925f428e2d21634a4
yuankang <647409429@qq.com>
2024-05-08 15:15:47 +0800

    feat: statemint_dota

--- site/src/utils/consts/assetInfoData/statemint/index.js
+++ site/src/utils/consts/assetInfoData/statemint/index.js
@@ -1,12 +1,14 @@
 import { assetUSDt } from "./usdt-1984";
 import { USDC } from "./usdc-1337";
 import { Ded } from "./ded-30";
+import { Dota } from "./dota-18";
 import { constructAssetId } from "../statemine";
 
 const statemintAssetInfo = {
   [constructAssetId(assetUSDt)]: assetUSDt.data,
   [constructAssetId(USDC)]: USDC.data,
   [constructAssetId(Ded)]: Ded.data,
+  [constructAssetId(Dota)]: Dota.data,
 };
 
 export default statemintAssetInfo;
