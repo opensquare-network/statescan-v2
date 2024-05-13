99bcd8e70b3739916e0b04f4e83ffc637bfc21a0
2nthony <ant.hu@outlook.com>
2024-05-13 15:11:30 +0800

    add tip for not started

--- site/src/components/vestings/table.jsx
+++ site/src/components/vestings/table.jsx
@@ -5,18 +5,36 @@ import AddressOrIdentity from "../address";
 import ValueDisplay from "../displayValue";
 import { ColoredLink } from "../styled/link";
 import Table from "../table";
+import { useSelector } from "react-redux";
+import { overviewSelector } from "../../store/reducers/socketSlice";
+import Tooltip from "../tooltip";
+
+function StartingBlockCell({ data }) {
+  const overview = useSelector(overviewSelector);
+  const startingBlock = data?.startingBlock;
+
+  const content = startingBlock.toLocaleString();
+
+  if (startingBlock > overview?.latestHeight) {
+    return <Tooltip tip="Not started">{content}</Tooltip>;
+  }
+
+  return (
+    <div>
+      <ColoredLink to={`/blocks/${startingBlock}`}>{content}</ColoredLink>
+    </div>
+  );
+}
 
 export default function VestingsTable({ data = [], loading }) {
   const { symbol, decimals } = useChainSettings();
 
   const tableData = data?.map?.((vesting) => {
     return [
-      <div key={vesting.address} style={{ display: "flex" }}>
+      <div style={{ display: "flex" }}>
         <AddressOrIdentity key={vesting.address} address={vesting.address} />
       </div>,
-      <ColoredLink to={`/blocks/${vesting.startingBlock}`}>
-        {vesting.startingBlock.toLocaleString()}
-      </ColoredLink>,
+      <StartingBlockCell data={vesting} />,
       <ValueDisplay
         value={toPrecision(vesting.perBlock, decimals)}
         symbol={symbol}
