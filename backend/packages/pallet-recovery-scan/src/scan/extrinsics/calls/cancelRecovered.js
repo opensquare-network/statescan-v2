7e4c253c20cbd6c36979212d9ca131dae8227b5e
Yongfeng LI <wliyongfeng@gmail.com>
2024-05-17 17:51:22 +0800

    Fix recovered cancel handling, #904

--- backend/packages/pallet-recovery-scan/src/scan/extrinsics/calls/cancelRecovered.js
+++ backend/packages/pallet-recovery-scan/src/scan/extrinsics/calls/cancelRecovered.js
@@ -2,8 +2,14 @@ const { setProxyHeightMark } = require("../../../store/proxy");
 const {
   store: { setKnownHeightMark },
 } = require("@statescan/common");
+const { getRecoverySection } = require("../../common/section");
 
 async function handleCancelRecovered(call, signer, extrinsicIndexer) {
+  const { section, method } = call;
+  if (getRecoverySection() !== section || "cancelRecovered" !== method) {
+    return;
+  }
+
   setKnownHeightMark(extrinsicIndexer.blockHeight);
   setProxyHeightMark(extrinsicIndexer.blockHeight);
 }
