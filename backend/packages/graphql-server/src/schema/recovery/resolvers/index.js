378a0414f4e27a1b36cfc45f81ca616eabaabe73
Yongfeng LI <wliyongfeng@gmail.com>
2024-05-19 14:28:09 +0800

    Add recoverable to queries, #904

--- backend/packages/graphql-server/src/schema/recovery/resolvers/index.js
+++ backend/packages/graphql-server/src/schema/recovery/resolvers/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   ...require("./recoveryScanHeight"),
   ...require("./recoverables"),
+  ...require("./recoverable"),
 };
--- backend/packages/graphql-server/src/schema/recovery/types/query.js
+++ backend/packages/graphql-server/src/schema/recovery/types/query.js
@@ -1,6 +1,7 @@
 const queries = /* GraphQL */ `
   type Query {
     recoverables(offset: Int!, limit: Int!): PagedRecoverable!
+    recoverable(height: Int!, lostAccount: String!): Recoverable
     recoveryScanHeight: Int
   }
 `;
