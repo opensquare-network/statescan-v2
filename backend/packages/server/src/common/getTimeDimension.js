fa28bbf086716d942d00100d92db32481f093c0a
chaojun <chaojun@opensquare.network>
2024-05-22 11:40:20 +0800

    fix: handle empty time dimension, #795

--- backend/packages/server/src/common/getTimeDimension.js
+++ backend/packages/server/src/common/getTimeDimension.js
@@ -8,67 +8,81 @@ function convertToNumber(value, valueName) {
   return number;
 }
 
+function getBlockHeightDimension(ctx) {
+  const { block_start, block_end } = ctx.query;
+  if (!block_start && !block_end) {
+    return {};
+  }
+  if (block_start && !block_end) {
+    return {
+      "indexer.blockHeight": {
+        $gte: convertToNumber(block_start, "Block start"),
+      },
+    };
+  }
+  if (!block_start && block_end) {
+    return {
+      "indexer.blockHeight": {
+        $lte: convertToNumber(block_end, "Block end"),
+      },
+    };
+  }
+  return {
+    $and: [
+      {
+        "indexer.blockHeight": {
+          $gte: convertToNumber(block_start, "Block start"),
+        },
+      },
+      {
+        "indexer.blockHeight": {
+          $lte: convertToNumber(block_end, "Block end"),
+        },
+      },
+    ],
+  };
+}
+
+function getDateDimension(ctx) {
+  const { date_start, date_end } = ctx.query;
+  if (!date_start && !date_end) {
+    return {};
+  }
+  if (date_start && !date_end) {
+    return {
+      "indexer.blockTime": {
+        $gte: convertToNumber(date_start, "Date start"),
+      },
+    };
+  }
+  if (!date_start && date_end) {
+    return {
+      "indexer.blockTime": { $lte: convertToNumber(date_end, "Date end") },
+    };
+  }
+  return {
+    $and: [
+      {
+        "indexer.blockTime": {
+          $gte: convertToNumber(date_start, "Date start"),
+        },
+      },
+      {
+        "indexer.blockTime": { $lte: convertToNumber(date_end, "Date end") },
+      },
+    ],
+  };
+}
+
 function getTimeDimension(ctx) {
   const { time_dimension } = ctx.query;
 
   if (time_dimension === "block") {
-    const { block_start, block_end } = ctx.query;
-    if (block_start && !block_end) {
-      return {
-        "indexer.blockHeight": {
-          $gte: convertToNumber(block_start, "Block start"),
-        },
-      };
-    }
-    if (!block_start && block_end) {
-      return {
-        "indexer.blockHeight": {
-          $lte: convertToNumber(block_end, "Block end"),
-        },
-      };
-    }
-    return {
-      $and: [
-        {
-          "indexer.blockHeight": {
-            $gte: convertToNumber(block_start, "Block start"),
-          },
-        },
-        {
-          "indexer.blockHeight": {
-            $lte: convertToNumber(block_end, "Block end"),
-          },
-        },
-      ],
-    };
+    return getBlockHeightDimension(ctx);
   }
 
   if (time_dimension === "date") {
-    const { date_start, date_end } = ctx.query;
-    if (date_start && !date_end) {
-      return {
-        "indexer.blockTime": {
-          $gte: convertToNumber(date_start, "Date start"),
-        },
-      };
-    }
-    if (!date_start && date_end) {
-      return {
-        "indexer.blockTime": { $lte: convertToNumber(date_end, "Date end") },
-      };
-    }
-    return {
-      $and: [
-        {
-          "indexer.blockTime": {
-            $gte: convertToNumber(date_start, "Date start"),
-          },
-        },
-        {
-          "indexer.blockTime": { $lte: convertToNumber(date_end, "Date end") },
-        },
-      ],
-    };
+    return getDateDimension(ctx);
   }
 
   return {};
