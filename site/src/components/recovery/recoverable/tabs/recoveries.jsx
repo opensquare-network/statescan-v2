df468f21132c20fe15a3a5f534dd33ae6102eb77
2nthony <ant.hu@outlook.com>
2024-05-27 17:59:41 +0800

    pagination

--- site/src/components/recovery/recoverable/tabs/recoveries.jsx
+++ site/src/components/recovery/recoverable/tabs/recoveries.jsx
@@ -1,13 +1,20 @@
 import { useRecoverableRecoveriesData } from "../../../../hooks/recovery/useRecoverableRecoveriesData";
-import { Panel } from "../../../styled/panel";
+import { useRecoverableParams } from "../../../../utils/hooks/recovery/useRecoverableParams";
+import Pagination from "../../../pagination";
+import { StyledPanelTableWrapper } from "../../../styled/panel";
 import RecoveriesTable from "../../recoveries/table";
 
 export default function RecoverableRecoveriesTab() {
+  const { page = 1 } = useRecoverableParams();
   const { data, loading } = useRecoverableRecoveriesData();
 
   return (
-    <Panel>
+    <StyledPanelTableWrapper
+      footer={
+        <Pagination page={page} pageSize={data?.limit} total={data?.total} />
+      }
+    >
       <RecoveriesTable loading={loading} data={data?.items} />
-    </Panel>
+    </StyledPanelTableWrapper>
   );
 }
