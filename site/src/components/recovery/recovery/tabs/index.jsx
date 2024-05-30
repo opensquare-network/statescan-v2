58776367728f25c1e68a6c9f5076c0dd6884bd59
2nthony <ant.hu@outlook.com>
2024-05-30 11:28:49 +0800

    add count

--- site/src/components/recovery/recovery/tabs/index.jsx
+++ site/src/components/recovery/recovery/tabs/index.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import DetailTabs from "../../../detail/tabs";
 import RecoveryTimelineTab from "./timeline";
 import RecoveryCallsTab from "./calls";
+import { useRecoveryCallsData } from "../../../../hooks/recovery/useRecoveryCallsData";
 
 export default function RecoveryDetailTabs() {
+  const { data } = useRecoveryCallsData();
+
   const tabs = [
     {
       name: "Timeline",
@@ -11,6 +14,7 @@ export default function RecoveryDetailTabs() {
     },
     {
       name: "Calls",
+      count: data?.total,
       children: <RecoveryCallsTab />,
     },
   ];
