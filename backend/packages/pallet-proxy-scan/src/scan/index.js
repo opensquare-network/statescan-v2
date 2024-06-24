1912da670d31554dd08fc2c46bdbcdbf05d00978
Yongfeng LI <wliyongfeng@gmail.com>
2024-06-24 16:31:55 +0800

    Scan known heights first if set, #919

--- backend/packages/pallet-proxy-scan/src/scan/index.js
+++ backend/packages/pallet-proxy-scan/src/scan/index.js
@@ -6,13 +6,22 @@ const {
   chain: { wrapBlockHandler },
   scan: { oneStepScan },
   utils: { sleep },
+  env: { firstScanKnowHeights },
 } = require("@osn/scan-common");
 const { handleBlock } = require("./block");
+const {
+  known: { scanKnownHeights },
+} = require("@statescan/common");
 
 async function scan() {
   const db = await getProxyDb();
   let toScanHeight = await db.getNextScanHeight();
   await deleteFrom(toScanHeight);
+
+  if (firstScanKnowHeights()) {
+    await scanKnownHeights(toScanHeight, wrapBlockHandler(handleBlock));
+    toScanHeight = await db.getNextScanHeight();
+  }
 
   /*eslint no-constant-condition: ["error", { "checkLoops": false }]*/
   while (true) {
