9d0a6a5e665b58b4dad173d019b34c65f3786730
Yongfeng LI <wliyongfeng@gmail.com>
2024-07-09 23:10:46 +0800

    Fix account all script for evm address, #925

--- backend/packages/account-scan/src/scripts/all/nomalize.js
+++ backend/packages/account-scan/src/scripts/all/nomalize.js
@@ -1,9 +1,7 @@
 const { normalizeData } = require("../../utils/normalizeAccountData");
-const { encodeAddress } = require("@polkadot/util-crypto");
 
-function normalizeEntry([key, value], ss58Format) {
-  const pubKeyU8a = key.slice(48);
-  const address = encodeAddress(pubKeyU8a, ss58Format);
+function normalizeEntry([key, value]) {
+  const address = key.args[0].toString();
   const detail = value.toJSON();
   const data = normalizeData(detail.data);
 
