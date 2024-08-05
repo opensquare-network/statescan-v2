c7ab4ea5bbdb8894cc591056abe4bd95ee414fa7
2nthony <ant.hu@outlook.com>
2024-08-05 15:02:28 +0800

    fix: delay blocks/time

--- site/src/components/table/body/delay.jsx
+++ site/src/components/table/body/delay.jsx
@@ -1,20 +1,32 @@
+import isNil from "lodash.isnil";
 import styled from "styled-components";
 import { Inter_14_500 } from "../../../styles/text";
 import { delayColumnType } from "../../../utils/constants";
+import { useChainBlockTime } from "../../../utils/hooks/chain/useChainBlockTime";
+import { timeRemain } from "../../../utils/viewFuncs/time";
 
 const Wrapper = styled.span`
   white-space: nowrap;
   ${Inter_14_500};
-  color: var(--fontTertiary);
 `;
 
 export default function DelayBody({ delayType, blocks }) {
+  const blockTime = useChainBlockTime();
+  const ms = (blocks || 0) * blockTime;
+
+  if (isNil(blocks)) {
+    return null;
+  }
+
+  if (blocks <= 0) {
+    return <Wrapper style={{ color: "var(--fontTertiary)" }}>-</Wrapper>;
+  }
+
   return (
     <Wrapper>
       {delayType === delayColumnType.blocks
         ? blocks?.toLocaleString?.()
-        : // TODO: proxy, blocktime for each chain settings
-          ""}
+        : timeRemain(ms)}
     </Wrapper>
   );
 }
