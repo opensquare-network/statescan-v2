4a7c496e539d4906fba479ffcaca6e3e698a3462
2nthony <hi@2nthony.com>
2024-08-07 15:10:41 +0800

    proxy announcement detail #919
    * extract colors
    
    * proxy announcement detail

---
+++ site/src/components/proxy/tabs/announcements/listData.jsx
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import DetailedBlock from "../../../detail/block";
+import List from "../../../list";
+import { Overpass_Mono_14_500 } from "../../../../styles/text";
+import { TagThemed } from "../../../tag";
+import { TextTertiary } from "../../../styled/text";
+import { PROXY_ANNOUNCEMENT_STATUS } from "../../../../utils/constants";
+import { PROXY_ANNOUNCEMENT_STATUS_COLORS } from "../../consts";
+
+const CallHash = styled.div`
+  ${Overpass_Mono_14_500}
+`;
+
+export default function ProxyAnnouncementListData({
+  loading,
+  callHash,
+  indexer,
+  normalizedCall,
+  status,
+  removedAt,
+  executedAt,
+  rejectedAt,
+}) {
+  let finalIndexer;
+  if (status === PROXY_ANNOUNCEMENT_STATUS.EXECUTED) {
+    finalIndexer = executedAt;
+  } else if (status === PROXY_ANNOUNCEMENT_STATUS.REJECTED) {
+    finalIndexer = rejectedAt;
+  } else if (status === PROXY_ANNOUNCEMENT_STATUS.REMOVED) {
+    finalIndexer = removedAt;
+  }
+  // TODO: proxy, killed?
+  else if (status === PROXY_ANNOUNCEMENT_STATUS.KILLED) {
+  }
+
+  const data = [
+    {
+      label: "Call Hash",
+      // TODO: proxy, call hash link?
+      value: <CallHash>{callHash}</CallHash>,
+    },
+    {
+      label: "Created At",
+      value: <DetailedBlock blockHeight={indexer?.blockHeight} />,
+    },
+    {
+      label: "Normalized Call",
+      value: normalizedCall ? (
+        <TagThemed>{normalizedCall?.method}</TagThemed>
+      ) : (
+        <TextTertiary>--</TextTertiary>
+      ),
+    },
+    {
+      type: "divider",
+    },
+    {
+      label: "Status",
+      value: (
+        <div style={{ color: PROXY_ANNOUNCEMENT_STATUS_COLORS[status] }}>
+          {status}
+        </div>
+      ),
+    },
+    finalIndexer && {
+      label: `${status} At`,
+      value: <DetailedBlock blockHeight={finalIndexer?.blockHeight} />,
+    },
+  ].filter(Boolean);
+
+  return <List data={loading ? [] : data} />;
+}
--- site/src/pages/proxy/announcement.jsx
+++ site/src/pages/proxy/announcement.jsx
@@ -1,9 +1,47 @@
+import { useParams } from "react-router-dom";
+import BreadCrumb from "../../components/breadCrumb";
 import DetailLayout from "../../components/layout/detailLayout";
+import { useProxyAnnouncementData } from "../../hooks/proxy/useProxyAnnouncementData";
+import ProxyAnnouncementListData from "../../components/proxy/tabs/announcements/listData";
+import styled from "styled-components";
+import { Panel } from "../../components/styled/panel";
+import { Inter_14_500 } from "../../styles/text";
+
+const StyledPanel = styled(Panel)`
+  ${Inter_14_500}
+`;
 
 export default function ProxyAnnouncementDetailPage() {
+  const { id } = useParams();
+  const { data, loading } = useProxyAnnouncementData(id);
+
   return (
-    <DetailLayout>
-      <div>Announcement</div>
+    <DetailLayout
+      breadCrumb={
+        <BreadCrumb
+          data={[
+            { name: "Proxy", path: "/proxy" },
+            { name: "Announcements" },
+            {
+              name: id,
+            },
+          ]}
+        />
+      }
+    >
+      <StyledPanel>
+        <ProxyAnnouncementListData
+          loading={loading}
+          callHash={data?.callHash}
+          indexer={data?.indexer}
+          normalizedCall={data?.normalizedCall}
+          status={data?.state}
+          isFinal={data?.isFinal}
+          removedAt={data?.removedAt}
+          executedAt={data?.executedAt}
+          rejectedAt={data?.rejectedAt}
+        />
+      </StyledPanel>
     </DetailLayout>
   );
 }
