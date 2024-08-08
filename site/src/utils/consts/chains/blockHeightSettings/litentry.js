b06f366f800d6455f9f69d834c6e5a8fcd312ee3
2nthony <ant.hu@outlook.com>
2024-08-08 15:44:23 +0800

    add blockheight range from 0

--- site/src/utils/consts/chains/blockHeightSettings/litentry.js
+++ site/src/utils/consts/chains/blockHeightSettings/litentry.js
@@ -1,4 +1,7 @@
 export const litentryBlockHeightSettings = {
+  0: {
+    decimals: 12,
+  },
   5492090: {
     decimals: 18,
   },
--- site/src/utils/consts/chains/blockHeightSettings/litmus.js
+++ site/src/utils/consts/chains/blockHeightSettings/litmus.js
@@ -1,4 +1,7 @@
 export const litmusBlockHeightSettings = {
+  0: {
+    decimals: 12,
+  },
   5717038: {
     decimals: 18,
   },
