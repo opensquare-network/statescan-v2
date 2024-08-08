1bd0e39105231d644320c423f85769fdd8573b63
2nthony <ant.hu@outlook.com>
2024-08-08 15:46:23 +0800

    sort heights

--- site/src/utils/hooks/chain/useChainSettings.js
+++ site/src/utils/hooks/chain/useChainSettings.js
@@ -12,7 +12,10 @@ export default function useChainSettings(blockHeight) {
 
   useEffect(() => {
     if (settings.blockHeightSettings && !isNil(blockHeight)) {
-      const heights = Object.keys(settings.blockHeightSettings).map(Number);
+      const heights = Object.keys(settings.blockHeightSettings)
+        .map(Number)
+        // descending block heights
+        .sort((a, b) => b - a);
 
       const blockHeightSettingsPatch =
         settings.blockHeightSettings[
