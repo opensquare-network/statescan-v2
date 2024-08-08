33944db2c8d1de29f9e110199014e798b0a545df
2nthony <ant.hu@outlook.com>
2024-08-08 15:33:37 +0800

    useChainSettings apply blockHeightSettings

--- site/src/utils/hooks/chain/useChainSettings.js
+++ site/src/utils/hooks/chain/useChainSettings.js
@@ -1,11 +1,34 @@
 import { getEnvChain } from "../../env";
 import chains from "../../consts/chains";
+import isNil from "lodash.isnil";
+import { useEffect, useState } from "react";
 
-/**
- * @returns {typeof chains.kusama}
- */
-export default function useChainSettings() {
+export default function useChainSettings(blockHeight) {
   const chain = getEnvChain();
+  /**
+   * @type {[typeof chains.kusama]}
+   */
+  const [settings, setSettings] = useState(chains[chain]);
 
-  return chains[chain];
+  useEffect(() => {
+    if (settings.blockHeightSettings && !isNil(blockHeight)) {
+      const heights = Object.keys(settings.blockHeightSettings).map(Number);
+
+      const blockHeightSettingsPatch =
+        settings.blockHeightSettings[
+          heights.find((height) => blockHeight >= height)
+        ];
+
+      if (blockHeightSettingsPatch) {
+        setSettings((s) => {
+          return {
+            ...s,
+            ...blockHeightSettingsPatch,
+          };
+        });
+      }
+    }
+  }, [blockHeight, settings.blockHeightSettings]);
+
+  return settings;
 }
