91fb89364487f990378b75c1a5b139a1a10b2aaa
2nthony <ant.hu@outlook.com>
2024-08-20 15:10:52 +0800

    fix: announcement removed icon

--- site/src/components/proxy/tabs/announcements/tabs/timelineIcon.jsx
+++ site/src/components/proxy/tabs/announcements/tabs/timelineIcon.jsx
@@ -4,6 +4,7 @@ export default function ProxyAnnouncementTimleineIcon({ name }) {
     Killed: "/imgs/icons/timeline-announcement_killed.svg",
     Executed: "/imgs/icons/timeline-announcement_executed.svg",
     Rejected: "/imgs/icons/timeline-announcement_rejected.svg",
+    Removed: "/imgs/icons/timeline-announcement_removed.svg",
   };
 
   return <img width={24} height={24} src={iconSrcMap[name]} alt={name} />;
