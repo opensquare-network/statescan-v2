a815f9d2351759a9e6454685a83d23272e6805c3
Yongfeng LI <wliyongfeng@gmail.com>
2024-08-21 15:21:16 +0800

    Fix pure proxy setting, #919

--- backend/packages/pallet-proxy-scan/src/play.js
+++ backend/packages/pallet-proxy-scan/src/play.js
@@ -11,7 +11,7 @@ const {
 (async () => {
   await initPalletProxyScanDb();
   await subscribeFinalizedHeight();
-  const blockHeights = [188889, 744556];
+  const blockHeights = [246631, 18644209];
 
   const api = await getApi();
   for (const height of blockHeights) {
