8bc90f38292ac2a049b6296cd123d48c87b9cce4
Yongfeng LI <wliyongfeng@gmail.com>
2024-08-23 10:31:03 +0800

    Remvove westend collectives

--- site/src/utils/consts/chains/index.js
+++ site/src/utils/consts/chains/index.js
@@ -3,7 +3,6 @@ import statemine from "./statemine";
 import statemint from "./statemint";
 import westmint from "./westmint";
 import litmus from "./litmus";
-import westendCollectives from "./westendCollectives";
 import collectives from "./collectives";
 import kusama from "./kusama";
 import polkadot from "./polkadot";
@@ -29,7 +28,6 @@ const chains = {
   statemine,
   statemint,
   westmint,
-  "westend-collectives": westendCollectives,
   collectives,
   // "hydradx-testnet": hydradxTestnet,
   // polimec,
