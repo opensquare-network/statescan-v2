0e0d15c0d56561a02a0edc493c2c76269fc78d07
Yongfeng LI <wliyongfeng@gmail.com>
2024-11-21 19:25:58 +0700

    Add cron job to update latest blocks and transfers (#978)
    * Add cron job to update blocks and transfers
    
    * Update cron

--- backend/packages/server/src/jobs/index.js
+++ backend/packages/server/src/jobs/index.js
@@ -1,9 +1,11 @@
 const { updateOverview } = require("./overview");
 const { queryAndSetSpecs } = require("./runtime");
+const { startCronJobs } = require("./cron");
 
 async function startJobs() {
   await updateOverview();
   await queryAndSetSpecs();
+  startCronJobs();
 }
 
 module.exports = {
