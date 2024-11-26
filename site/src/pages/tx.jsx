df202dfd00d0bf5dc5712a7d3433977f3671d4c7
2nthony <ant.hu@outlook.com>
2024-11-26 18:18:49 +0800

    clean

--- site/src/pages/tx.jsx
+++ site/src/pages/tx.jsx
@@ -3,14 +3,11 @@ import DetailLayout from "../components/layout/detailLayout";
 import { useTxData } from "../hooks/tx/useTxData";
 import TxEvmDetailContent from "../components/tx/evmDetailContent";
 import TxSubstrateDetailContent from "../components/tx/substrateDetailContent";
-import { useSelector } from "react-redux";
-import { finalizedHeightSelector } from "../store/reducers/chainSlice";
 import { hashEllipsis } from "../utils/viewFuncs/text";
 import { useParams } from "react-router-dom";
 
 export default function TXPage() {
   const { id = "" } = useParams();
-  const finalizedHeight = useSelector(finalizedHeightSelector);
 
   const { data } = useTxData();
   const breadCrumb = (
