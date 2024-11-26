fc44e809bfc13b67c6da5d5861a5a55c70f53aae
2nthony <ant.hu@outlook.com>
2024-11-26 11:36:04 +0800

    substrate tx detail panel

--- site/src/components/tx/substrateDetailPanel.jsx
+++ site/src/components/tx/substrateDetailPanel.jsx
@@ -1,3 +1,21 @@
-export default function TxSubstrateDetailPanel() {
-  return <div>TxSubstrateDetailPanel</div>;
+import { useMemo } from "react";
+import { toExtrinsicDetailItem } from "../../utils/viewFuncs/toDetailItem";
+import { Panel } from "../styled/panel";
+import List from "../list";
+
+export default function TxSubstrateDetailPanel({ data = {} }) {
+  const listData = useMemo(() => {
+    data.call = {
+      section: data?.section,
+      method: data?.method,
+    };
+
+    return toExtrinsicDetailItem(data);
+  }, [data]);
+
+  return (
+    <Panel>
+      <List data={listData} />
+    </Panel>
+  );
 }
--- site/src/pages/tx.jsx
+++ site/src/pages/tx.jsx
@@ -3,16 +3,23 @@ import DetailLayout from "../components/layout/detailLayout";
 import { useTxData } from "../hooks/tx/useTxData";
 import TxEvmDetailPanel from "../components/tx/evmDetailPanel";
 import TxSubstrateDetailPanel from "../components/tx/substrateDetailPanel";
+import { useSelector } from "react-redux";
+import { finalizedHeightSelector } from "../store/reducers/chainSlice";
+import { useMemo } from "react";
 
 export default function TXPage() {
+  const finalizedHeight = useSelector(finalizedHeightSelector);
+
   const breadCrumb = <BreadCrumb data={[{ name: "Transaction" }]} />;
   const { data } = useTxData();
 
   let content;
   if (data?.isEvm) {
-    content = <TxEvmDetailPanel data={data} />;
+    const isFinalized = data?.blockNumber <= finalizedHeight;
+    content = <TxEvmDetailPanel data={{ ...data, isFinalized }} />;
   } else {
-    content = <TxSubstrateDetailPanel data={data} />;
+    const isFinalized = data?.indexer?.blockHeight <= finalizedHeight;
+    content = <TxSubstrateDetailPanel data={{ ...data, isFinalized }} />;
   }
 
   return <DetailLayout breadCrumb={breadCrumb}>{content}</DetailLayout>;
