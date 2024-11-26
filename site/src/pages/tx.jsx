867d4ed0913ba61bc902fd229102818f350f1a36
2nthony <ant.hu@outlook.com>
2024-11-26 14:15:07 +0800

    add breadcrumb

--- site/src/pages/tx.jsx
+++ site/src/pages/tx.jsx
@@ -5,13 +5,24 @@ import TxEvmDetailPanel from "../components/tx/evmDetailPanel";
 import TxSubstrateDetailPanel from "../components/tx/substrateDetailPanel";
 import { useSelector } from "react-redux";
 import { finalizedHeightSelector } from "../store/reducers/chainSlice";
-import { useMemo } from "react";
+import { hashEllipsis } from "../utils/viewFuncs/text";
+import { useParams } from "react-router-dom";
 
 export default function TXPage() {
+  const { id = "" } = useParams();
   const finalizedHeight = useSelector(finalizedHeightSelector);
 
-  const breadCrumb = <BreadCrumb data={[{ name: "Transaction" }]} />;
   const { data } = useTxData();
+  const breadCrumb = (
+    <BreadCrumb
+      data={[
+        { name: "Transaction" },
+        {
+          name: hashEllipsis(id),
+        },
+      ]}
+    />
+  );
 
   let content;
   if (data?.isEvm) {
