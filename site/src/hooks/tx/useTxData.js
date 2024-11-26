c763d21c3dfe173c5c4c074e735d9c0fef9a8a30
2nthony <ant.hu@outlook.com>
2024-11-26 18:28:11 +0800

    handle tx notfound

--- site/src/hooks/tx/useTxData.js
+++ site/src/hooks/tx/useTxData.js
@@ -1,14 +1,31 @@
 import { useParams } from "react-router-dom";
 import { useAsync } from "react-use";
 import api from "../../services/api";
+import { useEffect } from "react";
+import {
+  clearHttpError,
+  handleApiError,
+} from "../../utils/viewFuncs/errorHandles";
+import { useDispatch } from "react-redux";
 
 export function useTxData() {
   const { id = "" } = useParams();
+  const dispatch = useDispatch();
 
-  const { value, loading } = useAsync(async () => {
+  const { value, loading, error } = useAsync(async () => {
     const resp = await api.fetch(`/txs/${id}`);
     return resp?.result;
   }, [id]);
+
+  useEffect(() => {
+    if (error) {
+      handleApiError(error, dispatch);
+    }
+
+    return () => {
+      clearHttpError(dispatch);
+    };
+  }, [error]);
 
   return {
     data: value,
