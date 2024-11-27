7d06777aabcfd23f5c4f9cf5f20fc340cd49035e
Yongfeng LI <wliyongfeng@gmail.com>
2024-11-27 12:44:27 +0800

    Fix identity scan height for kusama/polkadot

--- backend/packages/identity-server/src/schema/resolvers/scanHeight.js
+++ backend/packages/identity-server/src/schema/resolvers/scanHeight.js
@@ -1,9 +1,18 @@
 const {
   identity: { getIdentityDb },
 } = require("@statescan/mongo");
+const {
+  env: { currentChain },
+} = require("@osn/scan-common");
 
 async function scanHeight() {
   const db = await getIdentityDb();
+  if (["polkadot", "kusama"].includes(currentChain())) {
+    const col = await db.getStatusCol();
+    const heightInfo = await col.findOne({ name: "people-chain-scan-height" });
+    return parseInt(heightInfo.value);
+  }
+
   return db.getScanHeight();
 }
 
