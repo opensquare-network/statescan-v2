bf5680049d40c6b6560f433457000713291d6a42
Yongfeng LI <wliyongfeng@gmail.com>
2024-11-29 12:02:45 +0800

    Add indexer for evm tx

--- backend/packages/block-scan/src/scan/events/evm/tx/index.js
+++ backend/packages/block-scan/src/scan/events/evm/tx/index.js
@@ -7,26 +7,50 @@ const { queryAndGetReceipt } = require("./receipt");
 const {
   env: { currentChain },
 } = require("@osn/scan-common");
+const isNil = require("lodash.isnil");
 
-async function handleOneTransaction(tx) {
+function findExtrinsicIndex(txHash, events) {
+  const targetEvent = events.find((e) => {
+    const { event } = e;
+    const { section, method } = event;
+    if ("ethereum" !== section || "Executed" !== method) {
+      return false;
+    }
+
+    return event.data[2].toString() === txHash;
+  });
+  if (!targetEvent) {
+    return null;
+  }
+
+  return targetEvent.phase.isNone ? null : targetEvent.phase.value.toNumber();
+}
+
+async function handleOneTransaction(tx, blockIndexer, blockEvents) {
   const col = await getEvmTxCol();
   const receipt = await queryAndGetReceipt(tx.hash);
+  const extrinsicIndex = findExtrinsicIndex(tx.hash, blockEvents);
+  const indexer = {
+    ...blockIndexer,
+    ...(isNil(extrinsicIndex) ? {} : { extrinsicIndex }),
+  };
   await col.insertOne({
+    indexer,
     ...normalizeTx(tx),
     receipt,
   });
 }
 
-async function queryAndSaveEvmTxs(blockHeight) {
+async function queryAndSaveEvmTxs(blockIndexer, blockEvents) {
   if (!["laos"].includes(currentChain())) {
     return;
   }
 
   const web3 = getEvmWeb3Instance();
-  const block = await web3.eth.getBlock(blockHeight, true);
+  const block = await web3.eth.getBlock(blockIndexer.blockHeight, true);
 
   for (const transaction of block.transactions) {
-    await handleOneTransaction(transaction);
+    await handleOneTransaction(transaction, blockIndexer, blockEvents);
   }
 }
 
