ce64a8dd02545928a358d312a866b2e5ca9b6580
Yongfeng LI <wliyongfeng@gmail.com>
2025-01-23 12:07:59 +0800

    Support stagelight in server side, #988 (#989)

--- backend/packages/server/src/utils/consts/chains.js
+++ backend/packages/server/src/utils/consts/chains.js
@@ -29,6 +29,7 @@ const chains = Object.freeze({
   "bridgehub-kusama": "bridgehub-kusama",
   "bridgehub-westend": "bridgehub-westend",
   laos: "laos",
+  stagelight: "stagelight",
 });
 
 const ss58Format = Object.freeze({
@@ -60,6 +61,7 @@ const ss58Format = Object.freeze({
   [chains["bridgehub-polkadot"]]: 0,
   [chains["bridgehub-kusama"]]: 2,
   [chains["bridgehub-westend"]]: 42,
+  [chains.stagelight]: 42,
 });
 
 function getSs58Format(chain) {
@@ -86,6 +88,7 @@ const uniquesModuleChains = [
 const transferOnBlockChains = [
   // whose native token transfer records are in block-scan DB.
   chains.paseo,
+  chains.stagelight,
 ];
 
 module.exports = {
