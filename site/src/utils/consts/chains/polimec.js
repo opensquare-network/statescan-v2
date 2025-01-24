5237218715e09e8bea01006f750b43c091f22756
Yongfeng LI <wliyongfeng@gmail.com>
2025-01-24 14:19:07 +0800

    Update polimec nodes

--- site/src/utils/consts/chains/polimec.js
+++ site/src/utils/consts/chains/polimec.js
@@ -3,6 +3,22 @@ import { collectivesModules } from "./collectives";
 
 const nodes = [
   {
+    name: "Amforc",
+    url: "wss://polimec.rpc.amforc.com/",
+  },
+  {
+    name: "Helikon",
+    url: "wss://rpc.helikon.io/polimec",
+  },
+  {
+    name: "IBP1",
+    url: "wss://polimec.ibp.network/",
+  },
+  {
+    name: "IBP2",
+    url: "wss://polimec.dotters.network/",
+  },
+  {
     name: "Polimec Foundation",
     url: "wss://rpc.polimec.org",
   },
