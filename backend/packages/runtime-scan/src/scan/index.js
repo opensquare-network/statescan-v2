7896152dc1cf16065b8d13af7c0a45cd3ffa31cd
Yongfeng LI <wliyongfeng@gmail.com>
2025-02-28 12:35:48 +0800

    Set runtime begin scan height for cere testnet

--- backend/packages/runtime-scan/src/scan/index.js
+++ backend/packages/runtime-scan/src/scan/index.js
@@ -5,11 +5,23 @@ const {
 } = require("@statescan/mongo");
 const {
   utils: { sleep },
+  env: { currentChain },
 } = require("@osn/scan-common");
+
+const cereTestnetMinHeight = 891032;
+
+function fixScanStartHeight(height) {
+  if ("cere-testnet" === currentChain() && height < cereTestnetMinHeight) {
+    return cereTestnetMinHeight;
+  }
+
+  return height;
+}
 
 async function scan() {
   const db = getRuntimeDb();
   let toScanHeight = await db.getNextScanHeight();
+  toScanHeight = fixScanStartHeight(toScanHeight);
   await deleteFrom(toScanHeight);
 
   /*eslint no-constant-condition: ["error", { "checkLoops": false }]*/
