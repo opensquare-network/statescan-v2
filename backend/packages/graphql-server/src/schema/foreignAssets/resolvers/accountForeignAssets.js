aa3eb110c6abdd92ec7b889a5e03f3e0564000b9
Yongfeng LI <wliyongfeng@gmail.com>
2025-03-26 17:51:00 +0800

    Support querying account foreign assets, #1032 (#1048)

---
+++ backend/packages/graphql-server/src/schema/foreignAssets/resolvers/accountForeignAssets.js
@@ -0,0 +1,42 @@
+const {
+  foreignAsset: { getHolderCol, getAllForeignAssets },
+} = require("@statescan/mongo");
+
+async function getHoldersWithAsset(holders) {
+  const assets = await getAllForeignAssets();
+  return (holders || []).map((holder) => {
+    const asset = assets.find((asset) => asset.assetId === holder.assetId);
+    return {
+      ...holder,
+      asset,
+    };
+  });
+}
+
+async function accountForeignAssets(_, _args) {
+  const { offset, limit, address } = _args;
+  if (parseInt(limit) > 100) {
+    throw new Error("Over max page size 100");
+  }
+
+  const q = { address };
+  const col = await getHolderCol();
+  const holders = await col
+    .find(q, { projection: { _id: 0 } })
+    .sort({ assetId: 1 })
+    .skip(offset)
+    .limit(limit)
+    .toArray();
+  const total = await col.countDocuments(q);
+
+  return {
+    holders: await getHoldersWithAsset(holders),
+    offset,
+    limit,
+    total,
+  };
+}
+
+module.exports = {
+  accountForeignAssets,
+};
