feee8be56ad5c1fbaecbda8897767d34e5c0c0c0
Yongfeng LI <wliyongfeng@gmail.com>
2025-06-26 18:24:19 +0800

    Simplify rewarded record info, #1083

--- backend/packages/pallet-staking-scan/src/scan/events/staking/events/rewarded.js
+++ backend/packages/pallet-staking-scan/src/scan/events/staking/events/rewarded.js
@@ -1,36 +1,27 @@
 const {
   palletStaking: { insertStakingReward },
 } = require("@statescan/mongo");
-const { getStakingInfo, getCurrentEra } = require("../../../common/query");
-const { parseRewardDestination } = require("../../../common/utils");
+const { getCurrentEra } = require("../../../common/query");
 
 async function handleRewarded(event, indexer) {
+  // todo: 1. get who get the reward -> done
+  // todo: 2. get the validator // get the latest PayoutStarted event and find the validator_stash
+  // todo: 2-1.
+  // todo: 3. get the amount -> done
+  // todo: 4. get era -> solved
+  // todo: 5. time -> done
   const stash = event.data[0].toString();
   const dest = event.data[1];
   const amount = event.data[2].toString();
-
-  const { destType, destAccount } = parseRewardDestination(dest);
-
-  const currentEra = await getCurrentEra(indexer?.blockHash?.toString());
-
-  const stakingInfo = await getStakingInfo(
-    stash,
-    currentEra,
-    indexer?.blockHash?.toString(),
-  );
-
+  const currentEra = await getCurrentEra(indexer?.blockHash);
   const rewardId = `${indexer.blockHeight}-${indexer.eventIndex}`;
 
   await insertStakingReward({
     rewardId,
     stash,
+    dest,
     amount,
-    destType,
-    destAccount,
     era: currentEra,
-    isValidator: stakingInfo.isValidator,
-    exposure: stakingInfo.exposure,
-    nominatorInfo: stakingInfo.nominatorInfo,
     indexer,
   });
 }
