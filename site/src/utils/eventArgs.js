88082b50bf26018b1dde351d6e189f01d5a46ef7
leocs2417 <leochen.sat24@gmail.com>
2025-07-03 16:15:23 +0800

    Fix: add isAddress judgement for makeEventArgs

--- site/src/utils/eventArgs.js
+++ site/src/utils/eventArgs.js
@@ -1,5 +1,6 @@
 import AddressOrIdentity from "../components/address";
 import { bigNumberToLocaleString } from "./viewFuncs";
+import { isAddress } from "@polkadot/util-crypto";
 
 function cleanTemplateArgs(typeName) {
   let result = typeName;
@@ -14,10 +15,18 @@ export function makeEventArgs(event) {
   const fields = event?.args?.map((item) => {
     const fieldType = cleanTemplateArgs(item.typeName).split("::").pop();
     if (fieldType === "AccountId") {
-      return [
-        item.name,
-        <AddressOrIdentity key="0" address={item.value} ellipsis={false} />,
-      ];
+      try {
+        if (isAddress(item.value)) {
+          return [
+            item.name,
+            <AddressOrIdentity key="0" address={item.value} ellipsis={false} />,
+          ];
+        } else {
+          return [item.name, String(item.value)];
+        }
+      } catch (error) {
+        return [item.name, String(item.value)];
+      }
     } else if (fieldType === "BalanceOf") {
       return [item.name, bigNumberToLocaleString(item.value)];
     }
