f262491620503188bc8d3ef1877b6044cbebc153
leocs2417 <leochen.sat24@gmail.com>
2025-09-09 17:08:26 +0800

    DestCell, #1135

--- site/src/components/accountStaking/rewards/table.jsx
+++ site/src/components/accountStaking/rewards/table.jsx
@@ -1,6 +1,6 @@
 import { stakingRewardsHead } from "../../../utils/constants";
 import AddressOrIdentity from "../../address";
-import { Flex } from "../../styled/flex";
+import { FlexColumn } from "../../styled/flex";
 import Table from "../../table";
 import Tooltip from "../../tooltip";
 import ValueDisplay from "../../displayValue";
@@ -8,8 +8,35 @@ import { toPrecision } from "@osn/common";
 import useChainSettings from "../../../utils/hooks/chain/useChainSettings";
 import ExtrinsicLink from "../../extrinsic/link";
 import { ColoredLink } from "../../styled/link";
+import { TextSecondary } from "../../styled/text";
 
-// TODO: columns
+function DestCell({ dest }) {
+  if (!dest) {
+    return <TextSecondary>-</TextSecondary>;
+  }
+
+  if (dest.account) {
+    return (
+      <FlexColumn style={{ gap: 4 }}>
+        <TextSecondary>Account</TextSecondary>
+        <AddressOrIdentity address={dest.account} maxWidth={160} />
+      </FlexColumn>
+    );
+  }
+
+  let content = "-";
+
+  if ("staked" in dest || "stash" in dest) {
+    content = "Stash"; // todo: Stash account
+  }
+
+  if ("controller" in dest) {
+    content = "Controller"; // todo: Controller account
+  }
+
+  return <TextSecondary>{content}</TextSecondary>;
+}
+
 export default function AccountStakingRewardsTable({ data = [], loading }) {
   const { decimals, symbol } = useChainSettings();
 
@@ -24,7 +51,7 @@ export default function AccountStakingRewardsTable({ data = [], loading }) {
       </ColoredLink>,
       <ExtrinsicLink key={`${reward}-1`} indexer={reward.indexer} />,
       reward?.indexer?.blockTime,
-      <Flex>{"dest"}</Flex>, // todo
+      <DestCell dest={reward.dest} />,
       <Tooltip tip={reward.validator}>
         <AddressOrIdentity address={reward.validator} />
       </Tooltip>,
