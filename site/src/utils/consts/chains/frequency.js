bbc34d95b8f3cc608183c95dfff1d07c7fd0bd00
quinn-gao <quinnn.gao@gmail.com>
2025-10-09 17:43:06 +0800

    feat: add frequency config

---
+++ site/src/utils/consts/chains/frequency.js
@@ -0,0 +1,34 @@
+import { ReactComponent as FrequencyIcon } from "../../../components/icons/frequency.svg";
+import { ReactComponent as FrequencyLogo } from "../../../components/icons/logo/frequency.svg";
+
+const nodes = [
+  {
+    name: "Frequency 0",
+    url: "wss://0.rpc.frequency.xyz",
+  },
+  {
+    name: "Frequency 1",
+    url: "wss://1.rpc.frequency.xyz",
+  },
+  {
+    name: "OnFinality",
+    url: "wss://frequency-polkadot.api.onfinality.io",
+  },
+];
+
+const frequency = {
+  name: "Frequency",
+  icon: <FrequencyIcon />,
+  logo: FrequencyLogo,
+  chain: "polkadot",
+  value: "frequency",
+  symbol: "BFRQCY",
+  decimals: 8,
+  ss58Format: 90,
+  color: "#790e70",
+  colorSecondary: "rgba(121, 14, 112, 0.1)",
+  nodes,
+  useOnChainBlockData: true,
+};
+
+export default frequency;
