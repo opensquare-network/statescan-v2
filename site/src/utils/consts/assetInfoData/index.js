87e61ba5f50e636523f5b266dd5a4b26c84397d1
quinn-gao <quinnn.gao@gmail.com>
2025-10-09 15:52:31 +0800

    fix: remove polimec

--- site/src/utils/consts/assetInfoData/index.js
+++ site/src/utils/consts/assetInfoData/index.js
@@ -1,12 +1,9 @@
 import { statemineAssetInfo, statemineForeignAssetInfo } from "./statemine";
 import { statemintAssetInfo, statemintForeignAssetInfo } from "./statemint";
-import chains from "../chains";
-import { polimecAssetInfo, polimecForeignAssetInfo } from "./polimec";
 
 const assetInfoData = {
   statemine: { ...statemineAssetInfo, ...statemineForeignAssetInfo },
   statemint: { ...statemintAssetInfo, ...statemintForeignAssetInfo },
-  [chains.polimec.value]: { ...polimecAssetInfo, ...polimecForeignAssetInfo },
 };
 
 export default assetInfoData;
