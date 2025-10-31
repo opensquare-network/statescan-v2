cc2cb687cb35bd1943bcf45dc197f5af2f84edab
leocs2417 <leochen.sat24@gmail.com>
2025-10-31 18:28:14 +0800

    Init validators list, #1153

--- site/src/pages/staking/validators/index.jsx
+++ site/src/pages/staking/validators/index.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
 import { StyledPanelTableWrapper } from "../../../components/styled/panel";
 import BreadCrumb from "../../../components/breadCrumb";
 import Layout from "../../../components/layout";
+import { useValidatorsData } from "./useValidatorsData";
+import StakingValidatorsTable from "./table";
+import Pagination from "../../../components/pagination";
+import { useQueryParams } from "../../../hooks/useQueryParams";
+import { LIST_DEFAULT_PAGE_SIZE } from "../../../utils/constants";
 
 export default function StakingValidators() {
+  const { page = 1 } = useQueryParams();
+  const pageSize = LIST_DEFAULT_PAGE_SIZE;
+  const [data, setData] = useState(null);
+
+  const { loading } = useValidatorsData({
+    onCompleted: setData,
+  });
+
   return (
     <Layout>
       <BreadCrumb data={[{ name: "Validators" }]} />
-      <StyledPanelTableWrapper>validators page</StyledPanelTableWrapper>
+      <StyledPanelTableWrapper
+        footer={
+          <Pagination
+            page={page}
+            pageSize={pageSize}
+            total={data?.stakingValidators?.total}
+          />
+        }
+      >
+        <StakingValidatorsTable
+          data={data?.stakingValidators?.items}
+          loading={loading}
+        />
+      </StyledPanelTableWrapper>
     </Layout>
   );
 }
---
+++ site/src/pages/staking/validators/table.jsx
@@ -0,0 +1,40 @@
+import { stakingValidatorsHead } from "../../../utils/constants";
+import AddressOrIdentity from "../../../components/address";
+import Table from "../../../components/table";
+import Tooltip from "../../../components/tooltip";
+import ValueDisplay from "../../../components/displayValue";
+import { toPrecision } from "@osn/common";
+import useChainSettings from "../../../utils/hooks/chain/useChainSettings";
+
+export default function StakingValidatorsTable({ data = [], loading }) {
+  const { decimals, symbol } = useChainSettings();
+
+  const tableData = data?.map?.((validator, index) => {
+    return [
+      <Tooltip key={`validator-${index}`} tip={validator.address}>
+        <AddressOrIdentity address={validator.address} />
+      </Tooltip>,
+      <ValueDisplay
+        key={`validator-self-stake-${index}`}
+        value={toPrecision(validator.self_stake, decimals)}
+        symbol={symbol}
+        showNotEqualTooltip
+      />,
+      <ValueDisplay
+        key={`total-stake-${index}`}
+        value={toPrecision(validator.total_stake, decimals)}
+        symbol={symbol}
+        showNotEqualTooltip
+      />,
+      <span key={`nominators-${index}`}>{validator.nominator_count}</span>,
+      <span key={`commission-${index}`}>
+        {(parseFloat(validator.commission) / 10000000).toFixed(2)}%
+      </span>,
+      <span key={`active-${index}`}>{validator?.active ? "Yes" : "No"}</span>,
+    ];
+  });
+
+  return (
+    <Table heads={stakingValidatorsHead} data={tableData} loading={loading} />
+  );
+}
