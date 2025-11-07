e5eda85ddc4a187e4b241aa033599fd004cbedd6
Yongfeng LI <wliyongfeng@gmail.com>
2025-11-07 17:41:15 +0800

    Update westming endpoints

--- site/src/utils/consts/chains/westmint.js
+++ site/src/utils/consts/chains/westmint.js
@@ -24,10 +24,6 @@ const westmint = {
     { name: "Parity", url: "wss://westend-asset-hub-rpc.polkadot.io/" },
     { name: "Dwellir", url: "wss://asset-hub-westend-rpc.dwellir.com" },
     { name: "Dwellir Tunisia", url: "wss://westmint-rpc-tn.dwellir.com/" },
-    {
-      name: "Permanence DAO EU",
-      url: "wss://asset-hub-westend.rpc.permanence.io/",
-    },
   ],
   useOnChainBlockData: true,
 };
