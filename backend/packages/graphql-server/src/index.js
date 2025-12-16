002769cb4c22be2973530eef40b945dceca3d564
Chaojun Huang <hyifeng@gmail.com>
2025-12-16 17:21:00 +0800

    Add rate limit to yoga server (#1169)
    * Add rate limit to yoga server
    
    * fix
    
    * Update

--- backend/packages/graphql-server/src/index.js
+++ backend/packages/graphql-server/src/index.js
@@ -5,6 +5,7 @@ const { createYoga } = require("graphql-yoga");
 const { createServer } = require("http");
 const { createApis } = require("./chainApi");
 const { initDbs } = require("./init");
+const { rateLimitPlugin } = require("./rateLimitPlugin");
 
 const port = parseInt(process.env.PORT) || 7100;
 
@@ -15,7 +16,10 @@ async function main() {
   await createApis();
   console.log("api initialized");
 
-  const yoga = createYoga({ schema });
+  const yoga = createYoga({
+    schema,
+    plugins: [rateLimitPlugin()],
+  });
   const server = createServer(yoga);
   server.listen(port, () => {
     console.info(`Server is running on http://localhost:${port}/graphql`);
