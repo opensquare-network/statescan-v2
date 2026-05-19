439ab8d940145cdeda8ffa7df7fa6d1556e8ce0e
chaojun <chaojun@opensquare.network>
2026-05-19 12:40:17 +0800

    Add bulletin chain config

---
+++ site/src/utils/consts/chains/bulletin.js
@@ -0,0 +1,26 @@
+import { ReactComponent as Bulletin } from "../../../components/icons/bulletin.svg";
+
+const nodes = [
+  {
+    name: "Parity",
+    url: "wss://bulletin-rpc.polkadot.io",
+  },
+  {
+    name: "Spectrum",
+    url: "wss://spectrum-03.simplystaking.xyz/cG9sa2Fkb3QtMDMtOTFkMmYwZGYtcG9sa2Fkb3Q/9QbAeudedsupNA/polkadotbulletin/mainnet/",
+  },
+];
+
+const bulletin = {
+  name: "Bulletin",
+  icon: <Bulletin width={20} height={20} />,
+  value: "bulletin",
+  symbol: "DOT",
+  decimals: 10,
+  nodes,
+  color: "#6B2D84",
+  colorSecondary: "rgba(107, 45, 132, 0.1)",
+  useOnChainBlockData: true,
+};
+
+export default bulletin;
--- site/src/utils/consts/chains/index.js
+++ site/src/utils/consts/chains/index.js
@@ -32,6 +32,7 @@ import ajuna from "./ajuna";
 // import kintsugi from "./kintsugi";
 import frequency from "./frequency";
 import datahavenTestnet from "./datahaven-testnet";
+import bulletin from "./bulletin";
 
 const chains = {
   polkadot,
@@ -73,6 +74,7 @@ const chains = {
   // "fintra-testnet": fintraTestnet,
   frequency,
   "datahaven-testnet": datahavenTestnet,
+  bulletin,
 };
 
 export default chains;
