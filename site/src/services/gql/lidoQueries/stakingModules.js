e305ea6647facbac86aaa21fc21cdedbf9d05d0e
wolyshaw <wolyshaw@users.noreply.github.com>
2026-06-30 11:35:15 +0800

    clean query

--- site/src/services/gql/lidoQueries/stakingModules.js
+++ site/src/services/gql/lidoQueries/stakingModules.js
@@ -78,34 +78,6 @@ export const GET_LIDO_STAKING_MODULE_TOTALS = gql`
   }
 `;
 
-export const GET_LIDO_STAKING_ROUTER_ETH_DEPOSITEDS = gql`
-  query GetLidoStakingRouterETHDepositeds(
-    $first: Int!
-    $where: StakingRouterETHDeposited_filter
-    $orderBy: StakingRouterETHDeposited_orderBy
-    $orderDirection: OrderDirection
-  ) {
-    stakingRouterETHDepositeds(
-      first: $first
-      where: $where
-      orderBy: $orderBy
-      orderDirection: $orderDirection
-    ) {
-      amount
-      blockNumber
-      blockTime
-      id
-      logIndex
-      stakingModule {
-        id
-        name
-      }
-      stakingModuleId
-      txHash
-    }
-  }
-`;
-
 export const GET_LIDO_SERVER_STAKING_ROUTER_ETH_DEPOSITED = gql`
   query GetLidoServerStakingRouterEthDeposited(
     $stakingModuleId: Int
@@ -133,103 +105,6 @@ export const GET_LIDO_SERVER_STAKING_ROUTER_ETH_DEPOSITED = gql`
       total
       offset
       limit
-    }
-  }
-`;
-
-export const GET_LIDO_STAKING_MODULE_TIMELINES = gql`
-  query GetLidoStakingModuleTimelines(
-    $first: Int!
-    $where: StakingModuleTimeline_filter
-    $orderBy: StakingModuleTimeline_orderBy
-    $orderDirection: OrderDirection
-  ) {
-    stakingModuleTimelines(
-      first: $first
-      where: $where
-      orderBy: $orderBy
-      orderDirection: $orderDirection
-    ) {
-      blockNumber
-      blockTime
-      eventType
-      id
-      logIndex
-      stakingModuleAdded {
-        blockNumber
-        blockTime
-        createdBy
-        id
-        logIndex
-        name
-        stakingModule
-        stakingModuleId
-        txHash
-      }
-      stakingModuleFeesSet {
-        blockNumber
-        blockTime
-        id
-        logIndex
-        setBy
-        stakingModuleFee
-        stakingModuleId
-        treasuryFee
-        txHash
-      }
-      stakingModuleMaxDepositsPerBlockSet {
-        blockNumber
-        blockTime
-        id
-        logIndex
-        maxDepositsPerBlock
-        setBy
-        stakingModuleId
-        txHash
-      }
-      stakingModuleMinDepositBlockDistanceSet {
-        blockNumber
-        blockTime
-        id
-        logIndex
-        minDepositBlockDistance
-        setBy
-        stakingModuleId
-        txHash
-      }
-      stakingModuleShareLimitSet {
-        blockNumber
-        blockTime
-        id
-        logIndex
-        priorityExitShareThreshold
-        setBy
-        stakeShareLimit
-        stakingModuleId
-        txHash
-      }
-      stakingModuleTargetShareSet {
-        blockNumber
-        blockTime
-        id
-        logIndex
-        setBy
-        stakingModuleId
-        targetShare
-        txHash
-      }
-      stakingModuleStatusSet {
-        blockNumber
-        blockTime
-        id
-        logIndex
-        setBy
-        stakingModuleId
-        status
-        txHash
-      }
-      stakingModuleId
-      txHash
     }
   }
 `;
